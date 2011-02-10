065e0bfb2c08823fb413f17eac4321b0c98e0558
laurent vanni <lvanni@mymed2.sophia.inria.fr>
2011-02-10 12:29:22 +0100

    FIX: 	- Consistency level any may not be applied to read operations
        NEW:
    	- Consistency on Write = ANY
    	- Consistency on Read  = One
    
        _LV

--- backend/src/com/mymed/model/core/wrapper/Wrapper.java
+++ backend/src/com/mymed/model/core/wrapper/Wrapper.java
@@ -17,7 +17,9 @@ import com.mymed.model.datastructure.User;
  */
 public class Wrapper {
 	
-	public static ConsistencyLevel defaultLevel = ConsistencyLevel.ANY;
+	/** Default ConsistencyLevel */
+	public static ConsistencyLevel consistencyOnWrite = ConsistencyLevel.ANY;
+	public static ConsistencyLevel consistencyOnRead = ConsistencyLevel.ONE;
 
 	/* --------------------------------------------------------- */
 	/* User Profile Management */
@@ -33,40 +35,40 @@ public class Wrapper {
 			// USER NAME
 			String name = new String(node.getSimpleColumn("Mymed",
 					columnFamily, id, "name".getBytes("UTF8"),
-					defaultLevel), "UTF8");
+					consistencyOnRead), "UTF8");
 			// USER GENDER
 			String gender = new String(node.getSimpleColumn("Mymed",
 					columnFamily, id, "gender".getBytes("UTF8"),
-					defaultLevel), "UTF8");
+					consistencyOnRead), "UTF8");
 			// USER LOCALE
 			String locale = new String(node.getSimpleColumn("Mymed",
 					columnFamily, id, "locale".getBytes("UTF8"),
-					defaultLevel), "UTF8");
+					consistencyOnRead), "UTF8");
 			// USER UPTIME
 			String updated_time = new String(node.getSimpleColumn("Mymed",
 					columnFamily, id, "updated_time".getBytes("UTF8"),
-					defaultLevel), "UTF8");
+					consistencyOnRead), "UTF8");
 			// USER PROFILE
 			String profile = new String(node.getSimpleColumn("Mymed",
 					columnFamily, id, "profile".getBytes("UTF8"),
-					defaultLevel), "UTF8");
+					consistencyOnRead), "UTF8");
 			// USER PROFILE_PICTURE
 			String profile_picture = new String(node.getSimpleColumn("Mymed",
 					columnFamily, id, "profile_picture".getBytes("UTF8"),
-					defaultLevel), "UTF8");
+					consistencyOnRead), "UTF8");
 			// USER SOCIAL_NETWORK
 			String social_network = new String(node.getSimpleColumn("Mymed",
 					columnFamily, id, "social_network".getBytes("UTF8"),
-					defaultLevel), "UTF8");
+					consistencyOnRead), "UTF8");
 			if(social_network.equals("myMed")){
 				// USER EMAIL
 				String email = new String(node.getSimpleColumn("Mymed",
 						columnFamily, id, "email".getBytes("UTF8"),
-						defaultLevel), "UTF8");
+						consistencyOnRead), "UTF8");
 				// USER PASSWORD
 				String password = new String(node.getSimpleColumn("Mymed",
 						columnFamily, id, "password".getBytes("UTF8"),
-						defaultLevel), "UTF8");
+						consistencyOnRead), "UTF8");
 				return new User(id, name, gender, locale, updated_time, profile,
 						profile_picture, social_network, email, password);
 			} else {
@@ -90,46 +92,46 @@ public class Wrapper {
 			// USER ID
 			node.setSimpleColumn("Mymed", columnFamily, user.getId(), "id"
 					.getBytes("UTF8"), user.getId().getBytes("UTF8"),
-					defaultLevel);
+					consistencyOnWrite);
 			// USER NAME
 			node.setSimpleColumn("Mymed", columnFamily, user.getId(), "name"
 					.getBytes("UTF8"), user.getName().getBytes("UTF8"),
-					defaultLevel);
+					consistencyOnWrite);
 			// USER GENDER
 			node.setSimpleColumn("Mymed", columnFamily, user.getId(), "gender"
 					.getBytes("UTF8"), user.getGender().getBytes("UTF8"),
-					defaultLevel);
+					consistencyOnWrite);
 			// USER LOCALE
 			node.setSimpleColumn("Mymed", columnFamily, user.getId(), "locale"
 					.getBytes("UTF8"), user.getLocale().getBytes("UTF8"),
-					defaultLevel);
+					consistencyOnWrite);
 			// USER UPTIME
 			node.setSimpleColumn("Mymed", columnFamily, user.getId(),
 					"updated_time".getBytes("UTF8"), user.getUpdated_time()
-					.getBytes("UTF8"), defaultLevel);
+					.getBytes("UTF8"), consistencyOnWrite);
 			// USER PROFILE
 			node.setSimpleColumn("Mymed", columnFamily, user.getId(), "profile"
 					.getBytes("UTF8"), user.getProfile().getBytes("UTF8"),
-					defaultLevel);
+					consistencyOnWrite);
 			// USER PROFILE_PICTURE
 
 			node.setSimpleColumn("Mymed", columnFamily, user.getId(),
 					"profile_picture".getBytes("UTF8"), user
 					.getProfile_picture().getBytes("UTF8"),
-					defaultLevel);
+					consistencyOnWrite);
 			// USER SOCIAL_NETWORK
 			node.setSimpleColumn("Mymed", columnFamily, user.getId(),
 					"social_network".getBytes("UTF8"), user.getSocial_network()
-					.getBytes("UTF8"), defaultLevel);
+					.getBytes("UTF8"), consistencyOnWrite);
 			if(user.getSocial_network().equals("myMed")){
 				// USER EMAIL
 				node.setSimpleColumn("Mymed", columnFamily, user.getId(), "email"
 						.getBytes("UTF8"), user.getEmail().getBytes("UTF8"),
-						defaultLevel);
+						consistencyOnWrite);
 				// USER PASSWORD
 				node.setSimpleColumn("Mymed", columnFamily, user.getId(),
 						"password".getBytes("UTF8"), user.getPassword().getBytes(
-						"UTF8"), defaultLevel);
+						"UTF8"), consistencyOnWrite);
 			}
 		} catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
