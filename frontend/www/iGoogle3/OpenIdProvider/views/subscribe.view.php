2827d33d139f9d4e7967521e9f416e0cdcabeb5e
Bastien BLANCHARD <bastien.blanchard@inria.fr>
2011-06-01 14:57:26 +0200

    _FIX 	- user symbolic link for OpenIdProvider 	- correct jQuery load in OpenIdProvider
    	_BB

--- frontend/www/iGoogle3/OpenIdProvider/views/subscribe.view.php
+++ frontend/www/iGoogle3/OpenIdProvider/views/subscribe.view.php
@@ -1,16 +1,31 @@
-				<form action="" method="post">
+<?php 
+$date = date('Y-m-d', time()-3600*24*365*30);
+?>
+				<form action="<?=$this->httpScriptPath.'/'.PAGE_SUBSCRIBE?>" method="post">
 					<div>
 						<div>
-							<label for="login">E-mail&nbsp;:</label>
-							<input id="login" name="login" />
+							<label for="login">Nom d'utilisateur&nbsp;:</label>
+							<input id="login" name="login" required="required" />
 						</div>
 						<div>
 							<label for="password">Mot de Passe&nbsp;:</label>
-							<input id="password" name="password" type="password" />
+							<input id="password" name="password" type="password" required="required" />
 						</div>
 						<div>
 							<label for="password2">Retapez le Mot de Passe&nbsp;:</label>
-							<input id="password2" name="password2" type="password" />
+							<input id="password2" name="password2" type="password" required="required" />
+						</div>
+						<div>
+							<label for="email">E-mail&nbsp;:</label>
+							<input id="email" name="email" type="email" required="required" />
+						</div>
+						<div>
+							<label for="city">Ville&nbsp;:</label>
+							<input id="city" name="city" />
+						</div>
+						<div>
+							<label for="dob">Date de Naissance&nbsp;:</label>
+							<input id="dob" name="dob" value="<?=$date?>" placeholder="<?=$date?>" type="date" />
 						</div>
 					</div>
 					<button type="submit"><span>S'inscrire</span></button>
