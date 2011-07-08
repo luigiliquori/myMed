5d11a25da7b6cbf5b79bd3304650d26822425290
Milo Casagrande <milo.casagrande@inria.fr>
2011-07-08 14:09:38 +0200

    Fixed the MUserBean
    	* Added simple JavaDoc to the methods
    	* Fixed the euqals() method to use values that are certain of not
    	  being null
    	* Added the hashCode() methods, necessary when you override the euqals()
    	  one

--- backend/src/com/mymed/model/data/MUserBean.java
+++ backend/src/com/mymed/model/data/MUserBean.java
@@ -7,9 +7,6 @@ package com.mymed.model.data;
  */
 public final class MUserBean extends AbstractMBean {
 
-	/* --------------------------------------------------------- */
-	/* Attributes */
-	/* --------------------------------------------------------- */
 	/** USER_ID */
 	private String id = null;
 	/** AUTHENTICATION_ID */
@@ -36,192 +33,329 @@ public final class MUserBean extends AbstractMBean {
 	private String socialNetworkID = null;
 	private String socialNetworkName = null;
 
-	/* --------------------------------------------------------- */
-	/* Constructors */
-	/* --------------------------------------------------------- */
-	public MUserBean() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	/* --------------------------------------------------------- */
-	/* Override methods */
-	/* --------------------------------------------------------- */
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals()
+	 */
 	@Override
 	public boolean equals(final Object object) {
 
 		boolean returnValue = true;
 
-		if (!(object instanceof MUserBean)) {
-			returnValue = false;
-		} else {
+		if (object instanceof MUserBean) {
 			final MUserBean comparable = (MUserBean) object;
 
 			/*
 			 * We compare only a subsets of the field to check that two
-			 * MUserBean objects are the same
+			 * MUserBean objects are the same. These should be values that are
+			 * set for sure, and not null.
 			 */
-			returnValue &= getBirthday() == comparable.getBirthday();
 			returnValue &= getEmail().equals(comparable.getEmail());
 			returnValue &= getFirstName().equals(comparable.getFirstName());
-			returnValue &= getGender().equals(comparable.getGender());
 			returnValue &= getLastName().equals(comparable.getLastName());
 			returnValue &= getId().equals(comparable.getId());
 			returnValue &= getName().equals(comparable.getName());
+		} else {
+			returnValue = false;
 		}
 
 		return returnValue;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
 
-	/* --------------------------------------------------------- */
-	/* GETTER AND SETTER */
-	/* --------------------------------------------------------- */
+		result = prime * result + (int) (birthday ^ birthday >>> 32);
+		result = prime * result + (email == null ? 0 : email.hashCode());
+		result = prime * result + (firstName == null ? 0 : firstName.hashCode());
+		result = prime * result + (gender == null ? 0 : gender.hashCode());
+		result = prime * result + (id == null ? 0 : id.hashCode());
+		result = prime * result + (lastName == null ? 0 : lastName.hashCode());
+		result = prime * result + (name == null ? 0 : name.hashCode());
+
+		return result;
+	}
+
 	@Override
 	public String toString() {
 		return "User:\n" + super.toString();
 	}
 
+	/**
+	 * @return the id
+	 */
 	public String getId() {
 		return id;
 	}
 
-	public void setId(String id) {
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(final String id) {
 		this.id = id;
 	}
 
+	/**
+	 * @return the login
+	 */
 	public String getLogin() {
 		return login;
 	}
 
-	public void setLogin(String login) {
+	/**
+	 * @param login
+	 *            the login to set
+	 */
+	public void setLogin(final String login) {
 		this.login = login;
 	}
 
+	/**
+	 * @return the email
+	 */
 	public String getEmail() {
 		return email;
 	}
 
-	public void setEmail(String email) {
+	/**
+	 * @param email
+	 *            the email to set
+	 */
+	public void setEmail(final String email) {
 		this.email = email;
 	}
 
+	/**
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
 
-	public void setName(String name) {
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(final String name) {
 		this.name = name;
 	}
 
+	/**
+	 * @return the firstName
+	 */
 	public String getFirstName() {
 		return firstName;
 	}
 
-	public void setFirstName(String firstName) {
+	/**
+	 * @param firstName
+	 *            the firstName to set
+	 */
+	public void setFirstName(final String firstName) {
 		this.firstName = firstName;
 	}
 
+	/**
+	 * @return the lastName
+	 */
 	public String getLastName() {
 		return lastName;
 	}
 
-	public void setLastName(String lastName) {
+	/**
+	 * @param lastName
+	 *            the lastName to set
+	 */
+	public void setLastName(final String lastName) {
 		this.lastName = lastName;
 	}
 
+	/**
+	 * @return the link
+	 */
 	public String getLink() {
 		return link;
 	}
 
-	public void setLink(String link) {
+	/**
+	 * @param link
+	 *            the link to set
+	 */
+	public void setLink(final String link) {
 		this.link = link;
 	}
 
+	/**
+	 * @return the birthday
+	 */
 	public long getBirthday() {
 		return birthday;
 	}
 
-	public void setBirthday(long birthday) {
+	/**
+	 * @param birthday
+	 *            the birthday to set
+	 */
+	public void setBirthday(final long birthday) {
 		this.birthday = birthday;
 	}
 
+	/**
+	 * @return the hometown
+	 */
 	public String getHometown() {
 		return hometown;
 	}
 
-	public void setHometown(String hometown) {
+	/**
+	 * @param hometown
+	 *            the hometown to set
+	 */
+	public void setHometown(final String hometown) {
 		this.hometown = hometown;
 	}
 
+	/**
+	 * @return the gender
+	 */
 	public String getGender() {
 		return gender;
 	}
 
-	public void setGender(String gender) {
+	/**
+	 * @param gender
+	 *            the gender to set
+	 */
+	public void setGender(final String gender) {
 		this.gender = gender;
 	}
 
+	/**
+	 * @return the lastConnection
+	 */
 	public long getLastConnection() {
 		return lastConnection;
 	}
 
-	public void setLastConnection(long lastConnection) {
+	/**
+	 * @param lastConnection
+	 *            the lastConnection to set
+	 */
+	public void setLastConnection(final long lastConnection) {
 		this.lastConnection = lastConnection;
 	}
 
+	/**
+	 * @return the buddyList
+	 */
 	public String getBuddyList() {
 		return buddyList;
 	}
 
-	public void setBuddyList(String buddyList) {
+	/**
+	 * @param buddyList
+	 *            the buddyList to set
+	 */
+	public void setBuddyList(final String buddyList) {
 		this.buddyList = buddyList;
 	}
 
+	/**
+	 * @return the subscriptionList
+	 */
 	public String getSubscribtionList() {
 		return subscribtionList;
 	}
 
-	public void setSubscribtionList(String subscribtionList) {
+	/**
+	 * @param subscribtionList
+	 *            the subscriptionList to set
+	 */
+	public void setSubscribtionList(final String subscribtionList) {
 		this.subscribtionList = subscribtionList;
 	}
 
+	/**
+	 * @return the reputation
+	 */
 	public String getReputation() {
 		return reputation;
 	}
 
-	public void setReputation(String reputation) {
+	/**
+	 * @param reputation
+	 *            the reputation to set
+	 */
+	public void setReputation(final String reputation) {
 		this.reputation = reputation;
 	}
 
+	/**
+	 * @return the session
+	 */
 	public String getSession() {
 		return session;
 	}
 
-	public void setSession(String session) {
+	/**
+	 * @param session
+	 *            the session to set
+	 */
+	public void setSession(final String session) {
 		this.session = session;
 	}
 
+	/**
+	 * @return the interactionList
+	 */
 	public String getInteractionList() {
 		return interactionList;
 	}
 
-	public void setInteractionList(String interactionList) {
+	/**
+	 * @param interactionList
+	 *            the interactionList to set
+	 */
+	public void setInteractionList(final String interactionList) {
 		this.interactionList = interactionList;
 	}
 
+	/**
+	 * @return the socialNetworkID
+	 */
 	public String getSocialNetworkID() {
 		return socialNetworkID;
 	}
 
-	public void setSocialNetworkID(String socialNetworkID) {
+	/**
+	 * @param socialNetworkID
+	 *            the socialNetworkID to set
+	 */
+	public void setSocialNetworkID(final String socialNetworkID) {
 		this.socialNetworkID = socialNetworkID;
 	}
 
+	/**
+	 * @return the socialNetworkName
+	 */
 	public String getSocialNetworkName() {
 		return socialNetworkName;
 	}
 
-	public void setSocialNetworkName(String socialNetworkName) {
+	/**
+	 * @param socialNetworkName
+	 *            the socialNetworkName to set
+	 */
+	public void setSocialNetworkName(final String socialNetworkName) {
 		this.socialNetworkName = socialNetworkName;
 	}
 }
