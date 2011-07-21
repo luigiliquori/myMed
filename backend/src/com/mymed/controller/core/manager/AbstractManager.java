cba79e4606710010b3b2199c392e477be1775f8e
Milo Casagrande <milo.casagrande@inria.fr>
2011-07-21 11:16:45 +0200

    Fixed setter creation
    	* Fixed the setter creation through reflection: check that
    	  the fields are not static or final
    	* Fixed a problem with boolean values and setter creation:
    	  need to discuss about a convention for the attributes name

--- backend/src/com/mymed/controller/core/manager/AbstractManager.java
+++ backend/src/com/mymed/controller/core/manager/AbstractManager.java
@@ -1,11 +1,11 @@
 package com.mymed.controller.core.manager;
 
-import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Map;
 import java.util.Map.Entry;
+
+import javassist.Modifier;
 
 import com.mymed.controller.core.exception.InternalBackEndException;
 import com.mymed.controller.core.manager.storage.IStorageManager;
@@ -18,21 +18,16 @@ import com.mymed.utils.ClassType;
  * 
  */
 public abstract class AbstractManager extends ManagerValues {
-	/* --------------------------------------------------------- */
-	/* Attributes */
-	/* --------------------------------------------------------- */
+
+	private static final int PRIV_FIN = Modifier.PRIVATE + Modifier.FINAL;
+	private static final int PRIV_STAT_FIN = Modifier.PRIVATE + Modifier.STATIC + Modifier.FINAL;
+
 	protected IStorageManager storageManager;
 
-	/* --------------------------------------------------------- */
-	/* Constructor */
-	/* --------------------------------------------------------- */
 	public AbstractManager(final IStorageManager storageManager) {
 		this.storageManager = storageManager;
 	}
 
-	/* --------------------------------------------------------- */
-	/* public method */
-	/* --------------------------------------------------------- */
 	/**
 	 * Introspection
 	 * 
@@ -47,28 +42,72 @@ public abstract class AbstractManager extends ManagerValues {
 			for (final Entry<byte[], byte[]> arg : args.entrySet()) {
 				try {
 					final Field field = mbean.getClass().getDeclaredField(new String(arg.getKey(), "UTF8"));
+
+					/*
+					 * We check the value of the modifiers of the field: if the
+					 * field is private and final, or private static and final,
+					 * we skip it.
+					 */
+					final int modifiers = field.getModifiers();
+					if (modifiers == PRIV_FIN || modifiers == PRIV_STAT_FIN) {
+						continue;
+					}
+
 					final ClassType classType = ClassType.inferType(field.getGenericType());
-
-					final StringBuilder setterName = new StringBuilder(20);
-					setterName.append("set");
-					setterName.append(field.getName().substring(0, 1).toUpperCase());
-					setterName.append(field.getName().substring(1));
-
-					setterName.trimToSize();
-
-					final Method method = mbean.getClass().getMethod(setterName.toString(),
-					        classType.getPrimitiveType());
-
+					final String setterName = createSetterName(field, classType);
+					final Method method = mbean.getClass().getMethod(setterName, classType.getPrimitiveType());
 					final Object argument = ClassType.objectFromClassType(classType, arg.getValue());
 
 					method.invoke(mbean, argument);
 				} catch (final NoSuchFieldException e) {
-					System.out.println("\nWARNING: " + new String(arg.getKey(), "UTF8") + " is not an MUserBean Field");
+					System.out
+					        .println("\nWARNING: " + new String(arg.getKey(), "UTF8") + " is not PRIV_FIN bean field");
 				}
 			}
 		} catch (final Exception ex) {
 			throw new InternalBackEndException(ex);
 		}
+
 		return mbean;
+	}
+
+	/**
+	 * Create the name of the setter method based on the field name and its
+	 * class.
+	 * <p>
+	 * This is particularly useful due to the fact that boolean fields does not
+	 * have a normal setter name.
+	 * 
+	 * @param field
+	 *            the filed we want the setter method of
+	 * @param classType
+	 *            the class type of the field
+	 * @return the name of the setter method
+	 */
+	private String createSetterName(final Field field, final ClassType classType) {
+		final StringBuilder setterName = new StringBuilder(20);
+		final String fieldName = field.getName();
+
+		setterName.append("set");
+		String subName = fieldName;
+
+		/*
+		 * Check that the boolean field we are on does start with 'is'. This
+		 * should be the default prefix for boolean fields. In this case the
+		 * setter method will be based on the field name, but without the 'is'
+		 * prefix.
+		 */
+		if (classType.equals(ClassType.BOOL)) {
+			if (fieldName.startsWith("is")) {
+				subName = fieldName.substring(2, fieldName.length());
+			}
+		}
+
+		setterName.append(subName.substring(0, 1).toUpperCase());
+		setterName.append(subName.substring(1));
+
+		setterName.trimToSize();
+
+		return setterName.toString();
 	}
 }
