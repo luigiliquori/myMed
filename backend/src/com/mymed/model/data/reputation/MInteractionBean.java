3d433dec8a8b127b55a1f786f2c1e7ec934ef1bf
Milo Casagrande <milo.casagrande@inria.fr>
2011-08-08 17:25:52 +0200

    Added copy constructor, equals and hashcode
    	* Added copy constructor methods to clone an instance of an object
    	* Added equals and hashcode methods

--- backend/src/com/mymed/model/data/reputation/MInteractionBean.java
+++ backend/src/com/mymed/model/data/reputation/MInteractionBean.java
@@ -8,6 +8,10 @@ import com.mymed.model.data.AbstractMBean;
  * 
  */
 public class MInteractionBean extends AbstractMBean {
+	/**
+	 * Used for the calculation of the hash code
+	 */
+	private static final int PRIME = 31;
 
 	/** INTERACTION_ID */
 	private String id;
@@ -23,6 +27,39 @@ public class MInteractionBean extends AbstractMBean {
 	private int snooze;
 	/** INTERACTION_LIST_ID */
 	private String complexInteraction;
+
+	/**
+	 * Copy constructor.
+	 * <p>
+	 * Provide a clone of the passed MInteractionBean
+	 * 
+	 * @param toClone
+	 *            the interaction bean to clone
+	 */
+	protected MInteractionBean(final MInteractionBean toClone) {
+		id = toClone.getId();
+		application = toClone.getApplication();
+		producer = toClone.getProducer();
+		consumer = toClone.getConsumer();
+		start = toClone.getStart();
+		end = toClone.getEnd();
+		feedback = toClone.getFeedback();
+		snooze = toClone.getSnooze();
+		complexInteraction = toClone.getComplexInteraction();
+	}
+
+	/**
+	 * Create a new empty MInteractionBean
+	 */
+	public MInteractionBean() {
+		// Empty constructor, needed because of the copy constructor
+	}
+
+	@Override
+	public MInteractionBean clone() {
+		final MInteractionBean interactionBean = new MInteractionBean(this);
+		return interactionBean;
+	}
 
 	@Override
 	public String toString() {
@@ -162,5 +199,78 @@ public class MInteractionBean extends AbstractMBean {
 	 */
 	public void setComplexInteraction(final String complexInteraction) {
 		this.complexInteraction = complexInteraction;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+
+		int result = 1;
+		result = PRIME * result + (application == null ? 0 : application.hashCode());
+		result = PRIME * result + (complexInteraction == null ? 0 : complexInteraction.hashCode());
+		result = PRIME * result + (consumer == null ? 0 : consumer.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(feedback);
+		result = PRIME * result + (int) (temp ^ temp >>> 32);
+		result = PRIME * result + (id == null ? 0 : id.hashCode());
+		result = PRIME * result + (producer == null ? 0 : producer.hashCode());
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(final Object obj) {
+
+		boolean equal = false;
+
+		if (this == obj) {
+			equal = true;
+		} else if (obj instanceof MInteractionBean) {
+			final MInteractionBean comparable = (MInteractionBean) obj;
+
+			equal = true;
+
+			if (application == null && comparable.getApplication() != null) {
+				equal &= false;
+			} else {
+				equal &= application.equals(comparable.getApplication());
+			}
+
+			if (complexInteraction == null && comparable.getComplexInteraction() != null) {
+				equal &= false;
+			} else {
+				equal &= complexInteraction.equals(comparable.getComplexInteraction());
+			}
+
+			if (consumer == null && comparable.getConsumer() != null) {
+				equal &= false;
+			} else {
+				equal &= consumer.equals(comparable.getConsumer());
+			}
+
+			equal &= feedback == comparable.getFeedback();
+
+			if (id == null && comparable.getId() != null) {
+				equal &= false;
+			} else {
+				equal &= id.equals(comparable.getId());
+			}
+
+			if (producer == null && comparable.getProducer() != null) {
+				equal &= false;
+			} else {
+				equal &= producer.equals(comparable.getProducer());
+			}
+		}
+
+		return equal;
 	}
 }
