f46f3b4165ddbef03386e00b4761a16b53df7e1a
Milo Casagrande <milo.casagrande@inria.fr>
2011-08-16 18:46:03 +0200

    Implemented runnable interface

--- backend/src/com/mymed/tests/stress/UserStressTest.java
+++ backend/src/com/mymed/tests/stress/UserStressTest.java
@@ -1,9 +1,12 @@
 package com.mymed.tests.stress;
 
-import java.util.Collections;
+import java.io.File;
 import java.util.LinkedList;
 import java.util.List;
 
+import com.mymed.controller.core.manager.profile.ProfileManager;
+import com.mymed.controller.core.manager.storage.StorageManager;
+import com.mymed.model.core.configuration.WrapperConfiguration;
 import com.mymed.model.data.user.MUserBean;
 
 /**
@@ -13,9 +16,12 @@ import com.mymed.model.data.user.MUserBean;
  * @author Milo Casagrande
  * 
  */
-public class UserStressTest extends StressTestValues {
+public class UserStressTest extends StressTestValues implements Runnable {
 
-	private final List<MUserBean> userList = Collections.synchronizedList(new LinkedList<MUserBean>());
+	private final List<MUserBean> userList = new LinkedList<MUserBean>();
+
+	private StorageManager storageManager;
+	private ProfileManager profileManager;
 
 	public UserStressTest() {
 		MUserBean userBean;
@@ -26,20 +32,46 @@ public class UserStressTest extends StressTestValues {
 			userBean.setBuddyList(String.format(USR_LIST_ID, i));
 			userBean.setEmail(String.format(EMAIL, i));
 			userBean.setFirstName(String.format(FIRST_NAME, i));
+			userBean.setLastName(String.format(LAST_NAME, i));
 			userBean.setGender(getRandomGender());
 			userBean.setId(String.format(USR_ID, i));
 			userBean.setLastConnection(System.currentTimeMillis());
 			userBean.setLogin(String.format(LOGIN, i));
 			userBean.setName(String.format(NAME, i));
 			userBean.setSession(String.format(SESSION, i));
+			userBean.setSubscribtionList(String.format(SUB_LIST_ID, i));
 
 			userList.add(userBean);
 		}
 	}
+
+	public void setUp() {
+		try {
+			storageManager = new StorageManager(new WrapperConfiguration(new File(CONF_FILE)));
+			profileManager = new ProfileManager(storageManager);
+		} catch (final Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
 	/**
 	 * @return the userList
 	 */
 	public List<MUserBean> getUserList() {
 		return userList;
+	}
+
+	@Override
+	public void run() {
+		while (!userList.isEmpty()) {
+			final MUserBean user = ((LinkedList<MUserBean>) userList).pop();
+			try {
+				profileManager.create(user);
+			} catch (final Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+
+		System.err.println(userList.isEmpty());
 	}
 }
