1ae7a9c477ead5ab67d16e63f067f07337e30caa
Milo Casagrande <milo.casagrande@inria.fr>
2011-08-16 17:05:13 +0200

    Use syncronized LinkedList instead of ArrayList

--- backend/src/com/mymed/tests/stress/SessionStressTest.java
+++ backend/src/com/mymed/tests/stress/SessionStressTest.java
@@ -1,8 +1,8 @@
 package com.mymed.tests.stress;
 
-import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Random;
 
 import com.mymed.model.data.session.MSessionBean;
 
@@ -15,22 +15,22 @@ import com.mymed.model.data.session.MSessionBean;
  */
 public class SessionStressTest extends StressTestValues {
 
-	private final List<MSessionBean> sessionList = new ArrayList<MSessionBean>(NUMBER_OF_ELEMENTS);
+	private final List<MSessionBean> sessionList = Collections.synchronizedList(new LinkedList<MSessionBean>());
 
 	public SessionStressTest() {
 		MSessionBean sessionBean;
-
-		final Random rand = new Random(System.currentTimeMillis());
 
 		for (int i = 0; i < NUMBER_OF_ELEMENTS; i++) {
 			sessionBean = new MSessionBean();
 
 			sessionBean.setId(String.format(SESSION, i));
 			sessionBean.setIp(String.format(IP_ADDRESS, i));
-			sessionBean.setP2P(rand.nextBoolean());
-			sessionBean.setPort(rand.nextInt(65000));
+			sessionBean.setP2P(random.nextBoolean());
+			sessionBean.setPort(random.nextInt(65000));
 			sessionBean.setTimeout(System.currentTimeMillis());
 			sessionBean.setUser(String.format(USR_ID, i));
+
+			sessionList.add(sessionBean);
 		}
 	}
 
