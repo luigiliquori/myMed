55345428cd5cb114f676e699a9ab91e3465f10c4
Milo Casagrande <milo.casagrande@inria.fr>
2011-08-18 12:37:38 +0200

    Completely refactor the method
    	* Use a LinkedList as a queue, and fire up two threads for
    	  adding and removing users from the database

--- backend/src/com/mymed/tests/stress/StressTest.java
+++ backend/src/com/mymed/tests/stress/StressTest.java
@@ -1,58 +1,79 @@
 package com.mymed.tests.stress;
 
-import static org.junit.Assert.fail;
-
-import java.io.File;
-import java.util.List;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import java.util.LinkedList;
 
 import com.mymed.controller.core.exception.InternalBackEndException;
-import com.mymed.controller.core.manager.profile.ProfileManager;
-import com.mymed.controller.core.manager.session.SessionManager;
-import com.mymed.controller.core.manager.storage.StorageManager;
-import com.mymed.model.core.configuration.WrapperConfiguration;
-import com.mymed.model.data.session.MAuthenticationBean;
-import com.mymed.model.data.session.MSessionBean;
 import com.mymed.model.data.user.MUserBean;
 
-public class StressTest extends StressTestValues {
-
-	private List<MUserBean> userList;
-	private List<MSessionBean> sessionList;
-	private List<MAuthenticationBean> authList;
-
-	private SessionManager sessionManager;
-	private StorageManager storageManager;
-	private ProfileManager profileManager;
-
-	@Before
-	public void setUp() throws InternalBackEndException {
-		storageManager = new StorageManager(new WrapperConfiguration(new File(CONF_FILE)));
-		sessionManager = new SessionManager(storageManager);
-		profileManager = new ProfileManager(storageManager);
-
+public class StressTest {
+	public static void main(final String[] args) throws InternalBackEndException {
+		final LinkedList<MUserBean> usersList = new LinkedList<MUserBean>();
 		final UserStressTest userTest = new UserStressTest();
-		userList = userTest.getUserList();
-	}
 
-	@After
-	public void cleanUp() {
-		sessionManager = null;
-		storageManager = null;
-		profileManager = null;
-	}
+		try {
+			final Thread addUsers = new Thread("addUsers") {
+				@Override
+				public void run() {
+					synchronized (usersList) {
+						while (usersList.isEmpty()) {
+							final MUserBean user = userTest.createUserBean();
 
-	@Test
-	public void testUser() {
-		for (final MUserBean bean : userList) {
-			try {
-				profileManager.create(bean);
-			} catch (final Exception ex) {
-				fail(ex.getMessage());
-			}
+							if (user == null) {
+								break;
+							}
+
+							usersList.add(user);
+							userTest.createUser(user);
+							// usersList.pop();
+
+							usersList.notifyAll();
+							try {
+								usersList.wait();
+							} catch (final InterruptedException ex) {
+								ex.printStackTrace();
+							}
+						}
+
+						usersList.notifyAll();
+					}
+				}
+			};
+
+			final Thread removeUsers = new Thread("removeUsers") {
+				@Override
+				public void run() {
+					synchronized (usersList) {
+						while (usersList.isEmpty()) {
+							try {
+								usersList.wait();
+							} catch (final InterruptedException ex) {
+								ex.printStackTrace();
+							}
+						}
+
+						while (!usersList.isEmpty()) {
+							final MUserBean user = usersList.pop();
+							userTest.removeUser(user);
+							usersList.notifyAll();
+							try {
+								usersList.wait();
+							} catch (final InterruptedException ex) {
+								ex.printStackTrace();
+							}
+						}
+
+						System.err.println("End of Games!");
+						usersList.notifyAll();
+					}
+				}
+			};
+
+			System.err.println("Starting thread " + addUsers.getName());
+			addUsers.start();
+			System.err.println("Starting thread " + removeUsers.getName());
+			removeUsers.start();
+		} catch (final Exception ex) {
+			ex.printStackTrace();
 		}
 	}
 }
