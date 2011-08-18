ed814ea12a98b6ee27857383fb10e96cc92f101d
Milo Casagrande <milo.casagrande@inria.fr>
2011-08-18 17:36:11 +0200

    Refactor the class to run the tests
    	* Now the thread are created in an external class

--- backend/src/com/mymed/tests/stress/StressTest.java
+++ backend/src/com/mymed/tests/stress/StressTest.java
@@ -1,79 +1,19 @@
 package com.mymed.tests.stress;
 
-import java.util.LinkedList;
-
-import com.mymed.controller.core.exception.InternalBackEndException;
-import com.mymed.model.data.user.MUserBean;
-
+/**
+ * Class to run all the stress test on Cassandra. It is just necessary to run
+ * this class.
+ * 
+ * @author Milo Casagrande
+ * 
+ */
 public class StressTest {
-	public static void main(final String[] args) throws InternalBackEndException {
-		final LinkedList<MUserBean> usersList = new LinkedList<MUserBean>();
-		final UserStressTest userTest = new UserStressTest();
+	public static void main(final String[] args) {
+		// Perform the stress test, all in their own thread
+		// final UserThread userThread = new UserThread(false);
+		final SessionThread sessionThread = new SessionThread();
 
-		try {
-			final Thread addUsers = new Thread("addUsers") {
-				@Override
-				public void run() {
-					synchronized (usersList) {
-						while (usersList.isEmpty()) {
-							final MUserBean user = userTest.createUserBean();
-
-							if (user == null) {
-								break;
-							}
-
-							usersList.add(user);
-							userTest.createUser(user);
-							// usersList.pop();
-
-							usersList.notifyAll();
-							try {
-								usersList.wait();
-							} catch (final InterruptedException ex) {
-								ex.printStackTrace();
-							}
-						}
-
-						usersList.notifyAll();
-					}
-				}
-			};
-
-			final Thread removeUsers = new Thread("removeUsers") {
-				@Override
-				public void run() {
-					synchronized (usersList) {
-						while (usersList.isEmpty()) {
-							try {
-								usersList.wait();
-							} catch (final InterruptedException ex) {
-								ex.printStackTrace();
-							}
-						}
-
-						while (!usersList.isEmpty()) {
-							final MUserBean user = usersList.pop();
-							userTest.removeUser(user);
-							usersList.notifyAll();
-							try {
-								usersList.wait();
-							} catch (final InterruptedException ex) {
-								ex.printStackTrace();
-							}
-						}
-
-						System.err.println("End of Games!");
-						usersList.notifyAll();
-					}
-				}
-			};
-
-			System.err.println("Starting thread " + addUsers.getName());
-			addUsers.start();
-			System.err.println("Starting thread " + removeUsers.getName());
-			removeUsers.start();
-		} catch (final Exception ex) {
-			ex.printStackTrace();
-		}
+		// userThread.start();
+		sessionThread.start();
 	}
 }
