f77f116db66f0f3cba820aa1fd1904926406a5cb
Milo Casagrande <milo.casagrande@inria.fr>
2011-08-18 12:39:20 +0200

    Refactor the class
    	* Added a static counter to check how many user beans are created
    	* Refactor the constructor and the other methods

--- backend/src/com/mymed/tests/stress/UserStressTest.java
+++ backend/src/com/mymed/tests/stress/UserStressTest.java
@@ -1,8 +1,6 @@
 package com.mymed.tests.stress;
 
 import java.io.File;
-import java.util.LinkedList;
-import java.util.List;
 
 import com.mymed.controller.core.manager.profile.ProfileManager;
 import com.mymed.controller.core.manager.storage.StorageManager;
@@ -16,62 +14,84 @@ import com.mymed.model.data.user.MUserBean;
  * @author Milo Casagrande
  * 
  */
-public class UserStressTest extends StressTestValues implements Runnable {
+public class UserStressTest extends StressTestValues {
+	/*
+	 * A static counter needed to verify how many user bean are created
+	 */
+	private static int counter = 0;
 
-	private final List<MUserBean> userList = new LinkedList<MUserBean>();
-
-	private StorageManager storageManager;
 	private ProfileManager profileManager;
 
+	/**
+	 * Initialize the class and set up the {link ProfileManager}.
+	 */
 	public UserStressTest() {
-		MUserBean userBean;
+		super();
 
-		for (int i = 0; i < NUMBER_OF_ELEMENTS; i++) {
-			userBean = new MUserBean();
-			userBean.setBirthday(getRandomDate());
-			userBean.setBuddyList(String.format(USR_LIST_ID, i));
-			userBean.setEmail(String.format(EMAIL, i));
-			userBean.setFirstName(String.format(FIRST_NAME, i));
-			userBean.setLastName(String.format(LAST_NAME, i));
-			userBean.setGender(getRandomGender());
-			userBean.setId(String.format(USR_ID, i));
-			userBean.setLastConnection(System.currentTimeMillis());
-			userBean.setLogin(String.format(LOGIN, i));
-			userBean.setName(String.format(NAME, i));
-			userBean.setSession(String.format(SESSION, i));
-			userBean.setSubscribtionList(String.format(SUB_LIST_ID, i));
-
-			userList.add(userBean);
-		}
-	}
-
-	public void setUp() {
 		try {
-			storageManager = new StorageManager(new WrapperConfiguration(new File(CONF_FILE)));
-			profileManager = new ProfileManager(storageManager);
+			profileManager = new ProfileManager(new StorageManager(new WrapperConfiguration(new File(CONF_FILE))));
 		} catch (final Exception ex) {
 			ex.printStackTrace();
 		}
 	}
 
 	/**
-	 * @return the userList
+	 * Create a new user bean. The ID is a progressive number based on the value
+	 * {@code NUMBER_OF_ELEMENTS} defined in {@link StressTestValues}
+	 * 
+	 * @return the newly created user bean, or {@code null} if the maximum
+	 *         NUMBER_OF_ELEMENTS is reached
 	 */
-	public List<MUserBean> getUserList() {
-		return userList;
-	}
+	public MUserBean createUserBean() {
+		MUserBean userBean = null;
 
-	@Override
-	public void run() {
-		while (!userList.isEmpty()) {
-			final MUserBean user = ((LinkedList<MUserBean>) userList).pop();
-			try {
-				profileManager.create(user);
-			} catch (final Exception ex) {
-				ex.printStackTrace();
-			}
+		if (!(counter >= NUMBER_OF_ELEMENTS)) {
+			userBean = new MUserBean();
+
+			userBean.setBirthday(getRandomDate());
+			userBean.setBuddyList(String.format(USR_LIST_ID, counter));
+			userBean.setEmail(String.format(EMAIL, counter));
+			userBean.setFirstName(String.format(FIRST_NAME, counter));
+			userBean.setLastName(String.format(LAST_NAME, counter));
+			userBean.setGender(getRandomGender());
+			userBean.setId(String.format(USR_ID, counter));
+			userBean.setLastConnection(System.currentTimeMillis());
+			userBean.setLogin(String.format(LOGIN, counter));
+			userBean.setName(String.format(NAME, counter));
+			userBean.setSession(String.format(SESSION, counter));
+			userBean.setSubscribtionList(String.format(SUB_LIST_ID, counter));
 		}
 
-		System.err.println(userList.isEmpty());
+		counter++;
+
+		return userBean;
+	}
+
+	/**
+	 * Create the user in the dabatase
+	 * 
+	 * @param user
+	 *            the user bean to use
+	 */
+	public void createUser(final MUserBean user) {
+		try {
+			profileManager.create(user);
+		} catch (final Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	/**
+	 * Remove a user from the database
+	 * 
+	 * @param user
+	 *            the user bean to use
+	 */
+	public void removeUser(final MUserBean user) {
+		try {
+			profileManager.delete(user.getId());
+		} catch (final Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 }
