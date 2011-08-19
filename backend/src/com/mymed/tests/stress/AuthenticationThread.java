54c9ea09579d06ec5c7699b9c3123b2a01095381
Milo Casagrande <milo.casagrande@inria.fr>
2011-08-19 17:48:44 +0200

    Added and refactored class for the Authentication stress test

---
+++ backend/src/com/mymed/tests/stress/AuthenticationThread.java
@@ -0,0 +1,46 @@
+package com.mymed.tests.stress;
+
+import java.util.LinkedList;
+
+import com.mymed.model.data.AbstractMBean;
+import com.mymed.model.data.session.MAuthenticationBean;
+import com.mymed.model.data.user.MUserBean;
+
+public class AuthenticationThread extends Thread {
+
+	private final LinkedList<AbstractMBean[]> authList = new LinkedList<AbstractMBean[]>();
+	private final AuthenticationTest authTest = new AuthenticationTest();
+	private final Thread createAuthentication;
+
+	public AuthenticationThread() {
+		super();
+
+		createAuthentication = new Thread() {
+			@Override
+			public void run() {
+				System.err.println("Starting the create authentication thread...");
+
+				while (authList.isEmpty()) {
+					final AbstractMBean[] beanArray = authTest.createAuthenticationBean();
+
+					if (beanArray == null) {
+						break;
+					}
+
+					authList.add(beanArray);
+
+					final MAuthenticationBean authBean = (MAuthenticationBean) beanArray[0];
+					final MUserBean userBean = (MUserBean) beanArray[1];
+
+					authTest.createAuthentication(userBean, authBean);
+					authList.pop();
+				}
+			}
+		};
+	}
+
+	@Override
+	public void run() {
+		createAuthentication.start();
+	}
+}
