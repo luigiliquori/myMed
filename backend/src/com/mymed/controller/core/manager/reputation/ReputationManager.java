098ec16c6f5dbf5af7ae061ad3b05562afbd0b54
Milo Casagrande <milo.casagrande@inria.fr>
2011-08-19 10:35:16 +0200

    Removed unuesed variable assignment

--- backend/src/com/mymed/controller/core/manager/reputation/ReputationManager.java
+++ backend/src/com/mymed/controller/core/manager/reputation/ReputationManager.java
@@ -1,6 +1,5 @@
 package com.mymed.controller.core.manager.reputation;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import com.mymed.controller.core.exception.IOBackEndException;
@@ -11,37 +10,39 @@ import com.mymed.model.data.reputation.MInteractionBean;
 import com.mymed.model.data.reputation.MReputationBean;
 import com.mymed.utils.MConverter;
 
-public class ReputationManager extends AbstractManager implements
-		IReputationManager {
+/**
+ * Manage the reputation of a user
+ * 
+ * @author lvanni
+ * @author Milo Casagrande
+ * 
+ */
+public class ReputationManager extends AbstractManager implements IReputationManager {
 
 	public ReputationManager() throws InternalBackEndException {
 		this(new StorageManager());
 	}
 
-	public ReputationManager(final StorageManager storageManager)
-			throws InternalBackEndException {
+	public ReputationManager(final StorageManager storageManager) throws InternalBackEndException {
 		super(storageManager);
 	}
 
 	@Override
-	public MReputationBean read(final String producerID,
-			final String consumerID, final String applicationID)
-			throws InternalBackEndException, IOBackEndException {
+	public MReputationBean read(final String producerID, final String consumerID, final String applicationID)
+	        throws InternalBackEndException, IOBackEndException {
 		final MReputationBean reputationBean = new MReputationBean();
-		Map<byte[], byte[]> args = new HashMap<byte[], byte[]>();
-		args = storageManager.selectAll(CF_REPUTATION, producerID
-				+ applicationID);
+		final Map<byte[], byte[]> args = storageManager.selectAll(CF_REPUTATION, producerID + applicationID);
+
 		return (MReputationBean) introspection(reputationBean, args);
 	}
 
 	@Override
-	public void update(final MInteractionBean interaction, final double feedback)
-			throws InternalBackEndException, IOBackEndException {
+	public void update(final MInteractionBean interaction, final double feedback) throws InternalBackEndException,
+	        IOBackEndException {
 		// REMINDER:
 		// THE REPUTATION VALUE IS THE LAST GIVEN FEEDBACK
 
-		storageManager.insertColumn(CF_REPUTATION, interaction.getProducer()
-				+ interaction.getApplication(), "value", MConverter
-				.doubleToByteBuffer(feedback).array());
+		storageManager.insertColumn(CF_REPUTATION, interaction.getProducer() + interaction.getApplication(), "value",
+		        MConverter.doubleToByteBuffer(feedback).array());
 	}
 }
