01bfd54e0c7468db563b2bbc7123ae5c2b758562
Milo Casagrande <milo.casagrande@inria.fr>
2011-08-19 10:33:43 +0200

    Removed unused variable assignament, updated JavaDocs

--- backend/src/com/mymed/controller/core/manager/reputation/InteractionManager.java
+++ backend/src/com/mymed/controller/core/manager/reputation/InteractionManager.java
@@ -1,6 +1,5 @@
 package com.mymed.controller.core.manager.reputation;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import com.mymed.controller.core.exception.IOBackEndException;
@@ -9,44 +8,43 @@ import com.mymed.controller.core.manager.AbstractManager;
 import com.mymed.controller.core.manager.storage.StorageManager;
 import com.mymed.model.data.reputation.MInteractionBean;
 
-public class InteractionManager extends AbstractManager implements
-		IInteractionManager {
+/**
+ * Manage the reputation of a user
+ * 
+ * @author lvanni
+ * @author Milo Casagrande
+ * 
+ */
+public class InteractionManager extends AbstractManager implements IInteractionManager {
 
 	public InteractionManager() throws InternalBackEndException {
 		this(new StorageManager());
 	}
 
-	public InteractionManager(final StorageManager storageManager)
-			throws InternalBackEndException {
+	public InteractionManager(final StorageManager storageManager) throws InternalBackEndException {
 		super(storageManager);
 	}
 
 	@Override
-	public void create(final MInteractionBean interaction)
-			throws InternalBackEndException, IOBackEndException {
-		storageManager.insertSlice(CF_INTERACTION, interaction.getId(),
-				interaction.getAttributeToMap());
+	public void create(final MInteractionBean interaction) throws InternalBackEndException, IOBackEndException {
+		storageManager.insertSlice(CF_INTERACTION, interaction.getId(), interaction.getAttributeToMap());
 	}
 
 	@Override
-	public MInteractionBean read(final String interactionID)
-			throws InternalBackEndException, IOBackEndException {
-		Map<byte[], byte[]> args = new HashMap<byte[], byte[]>();
+	public MInteractionBean read(final String interactionID) throws InternalBackEndException, IOBackEndException {
 		final MInteractionBean interaction = new MInteractionBean();
-		args = storageManager.selectAll(CF_INTERACTION, interactionID);
+		final Map<byte[], byte[]> args = storageManager.selectAll(CF_INTERACTION, interactionID);
 
 		return (MInteractionBean) introspection(interaction, args);
 	}
 
 	@Override
-	public void update(final MInteractionBean interaction)
-			throws InternalBackEndException, IOBackEndException {
+	public void update(final MInteractionBean interaction) throws InternalBackEndException, IOBackEndException {
 		create(interaction);
 	}
 
 	@Override
-	public void delete(final String interactionID)
-			throws InternalBackEndException {
+	public void delete(final String interactionID) throws InternalBackEndException {
 		storageManager.removeAll(CF_INTERACTION, interactionID);
 	}
 }
