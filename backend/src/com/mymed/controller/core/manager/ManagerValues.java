b78b9fcfe6d90b7dc6552d8549b439d8a7c3d814
Milo Casagrande <milo.casagrande@inria.fr>
2011-08-26 14:43:22 +0200

    Added logging facilities
    	* First work to add logging facilities

--- backend/src/com/mymed/controller/core/manager/ManagerValues.java
+++ backend/src/com/mymed/controller/core/manager/ManagerValues.java
@@ -1,5 +1,9 @@
 package com.mymed.controller.core.manager;
 
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.Logger;
+
 /**
  * Simple class to hold generic string that can be used with all the managers.
  * We hold the names of the columns families and of the super columns
@@ -84,4 +88,25 @@ public class ManagerValues {
 	 * The 'RaterList' super column
 	 */
 	protected static final String SC_RATER_LIST = "RaterList";
+
+	// Logger facilities
+	/**
+	 * The name of the default info logger as defined in the file logback.xml
+	 */
+	private static final String INFO_LOG_NAME = "mymed.info.logger";
+
+	/**
+	 * The name of the default debug logger as defined in the file logback.xml
+	 */
+	private static final String DEBUG_LOG_NAME = "mymed.debug.logger";
+
+	/**
+	 * The default info logger to be used throughout all the managers
+	 */
+	protected static final Logger INFO_LOGGER = (Logger) LoggerFactory.getLogger(INFO_LOG_NAME);
+
+	/**
+	 * The default debug logger to be used throughout all the managers
+	 */
+	protected static final Logger DEBUG_LOGGER = (Logger) LoggerFactory.getLogger(DEBUG_LOG_NAME);
 }
