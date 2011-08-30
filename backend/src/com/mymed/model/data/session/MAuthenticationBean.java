c19c3e8d29070dbea3cdbe21e0bff584dd90fc96
Milo Casagrande <milo.casagrande@inria.fr>
2011-08-30 15:01:09 +0200

    Added preliminary hashCode and equals methods

--- backend/src/com/mymed/model/data/session/MAuthenticationBean.java
+++ backend/src/com/mymed/model/data/session/MAuthenticationBean.java
@@ -13,23 +13,18 @@ import com.mymed.model.data.user.MUserBean;
  */
 public class MAuthenticationBean extends AbstractMBean {
 
+	/*
+	 * Used for the calculation of the hashCode()
+	 */
+	private static final int PRIME = 31;
+
 	/** AUTHENTICATION_ID */
 	private String login = "";
 	/** USER_ID */
 	private String user = "";
 	/** sha256(string) */
 	private String password = "";
-	
-	/* --------------------------------------------------------- */
-	/* Constructors */
-	/* --------------------------------------------------------- */
-	public MAuthenticationBean() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	/* --------------------------------------------------------- */
-	/* Override methods */
-	/* --------------------------------------------------------- */
+
 	@Override
 	public String toString() {
 		return "Autentication:\n" + super.toString();
@@ -78,5 +73,60 @@ public class MAuthenticationBean extends AbstractMBean {
 	 */
 	public void setPassword(final String password) {
 		this.password = password;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		int result = 1;
+		result = PRIME * result + (login == null ? 0 : login.hashCode());
+		result = PRIME * result + (password == null ? 0 : password.hashCode());
+		result = PRIME * result + (user == null ? 0 : user.hashCode());
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof MAuthenticationBean)) {
+			return false;
+		}
+		final MAuthenticationBean other = (MAuthenticationBean) obj;
+		if (login == null) {
+			if (other.login != null) {
+				return false;
+			}
+		} else if (!login.equals(other.login)) {
+			return false;
+		}
+		if (password == null) {
+			if (other.password != null) {
+				return false;
+			}
+		} else if (!password.equals(other.password)) {
+			return false;
+		}
+		if (user == null) {
+			if (other.user != null) {
+				return false;
+			}
+		} else if (!user.equals(other.user)) {
+			return false;
+		}
+		return true;
 	}
 }
