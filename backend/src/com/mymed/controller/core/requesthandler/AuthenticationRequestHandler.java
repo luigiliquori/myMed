91d62f0e23aa8a41a0b39b33f6885d97b38907b7
Milo Casagrande <milo.casagrande@inria.fr>
2011-09-19 14:50:10 +0200

    Fixed logger and non-optimal switch constructs

--- backend/src/com/mymed/controller/core/requesthandler/AuthenticationRequestHandler.java
+++ backend/src/com/mymed/controller/core/requesthandler/AuthenticationRequestHandler.java
@@ -15,6 +15,7 @@ import com.mymed.controller.core.manager.authentication.AuthenticationManager;
 import com.mymed.controller.core.manager.authentication.IAuthenticationManager;
 import com.mymed.model.data.session.MAuthenticationBean;
 import com.mymed.model.data.user.MUserBean;
+import com.mymed.utils.MLogger;
 
 /**
  * Servlet implementation class AuthenticationRequestHandler
@@ -24,61 +25,84 @@ public class AuthenticationRequestHandler extends AbstractRequestHandler {
 	/* Attributes */
 	/* --------------------------------------------------------- */
 	private static final long serialVersionUID = 1L;
-	
+
 	private IAuthenticationManager authenticationManager;
-     
+
 	/* --------------------------------------------------------- */
 	/* Constructors */
 	/* --------------------------------------------------------- */
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AuthenticationRequestHandler() throws ServletException {
-    	super();
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public AuthenticationRequestHandler() throws ServletException {
+		super();
 		try {
-			this.authenticationManager = new AuthenticationManager();
-		} catch (InternalBackEndException e) {
+			authenticationManager = new AuthenticationManager();
+		} catch (final InternalBackEndException e) {
 			throw new ServletException("AuthenticationManager is not accessible because: " + e.getMessage());
 		}
-    }
+	}
 
-    /* --------------------------------------------------------- */
+	/* --------------------------------------------------------- */
 	/* extends HttpServlet */
 	/* --------------------------------------------------------- */
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	@Override
+	protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException,
+	        IOException {
 		try {
 			/** Get the parameters */
-			Map<String, String> parameters = getParameters(request);
+			final Map<String, String> parameters = getParameters(request);
 
 			/** Get the method code */
-			RequestCode code = requestCodeMap.get(parameters.get("code"));
+			final RequestCode code = requestCodeMap.get(parameters.get("code"));
 
 			/** handle the request */
 			String login, password;
-			if((login = parameters.get("login"))== null || (password = parameters.get("password"))== null){
+			if ((login = parameters.get("login")) == null || (password = parameters.get("password")) == null) {
 				handleError(new InternalBackEndException("missing argument!"), response);
 				return;
 			}
-			switch (code) {
-			case READ:
+
+			if (code == RequestCode.READ) {
 				MUserBean userBean;
 				try {
 					userBean = authenticationManager.read(login, password);
 					setResponseText(getGson().toJson(userBean));
-				} catch (IOBackEndException e) {
+				} catch (final IOBackEndException e) {
 					handleError(e, response);
 					return;
 				}
-				break;
-			default:
-				handleError(new InternalBackEndException("ProfileRequestHandler.doGet(" + code + ") not exist!"), response);
+			} else {
+				handleError(new InternalBackEndException("ProfileRequestHandler.doGet(" + code + ") not exist!"),
+				        response);
 				return;
 			}
+
+			// TODO test if it works
+			// switch (code) {
+			// case READ:
+			// MUserBean userBean;
+			// try {
+			// userBean = authenticationManager.read(login, password);
+			// setResponseText(getGson().toJson(userBean));
+			// } catch (IOBackEndException e) {
+			// handleError(e, response);
+			// return;
+			// }
+			// break;
+			// default:
+			// handleError(new
+			// InternalBackEndException("ProfileRequestHandler.doGet(" + code +
+			// ") not exist!"), response);
+			// return;
+			// }
+
 			super.doGet(request, response);
-		} catch (InternalBackEndException e) {
+		} catch (final InternalBackEndException e) {
 			e.printStackTrace();
 			handleError(e, response);
 			return;
@@ -86,82 +110,95 @@ public class AuthenticationRequestHandler extends AbstractRequestHandler {
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	@Override
+	protected void doPost(final HttpServletRequest request, final HttpServletResponse response)
+	        throws ServletException, IOException {
 		try {
 			/** Get the parameters */
-			Map<String, String> parameters = getParameters(request);
+			final Map<String, String> parameters = getParameters(request);
 
 			/** Get the method code */
-			RequestCode code = requestCodeMap.get(parameters.get("code"));
+			final RequestCode code = requestCodeMap.get(parameters.get("code"));
 
 			/** handle the request */
 			String authentication;
-			if((authentication = parameters.get("authentication"))== null){
+			if ((authentication = parameters.get("authentication")) == null) {
 				handleError(new InternalBackEndException("missing authentication argument!"), response);
 				return;
 			}
 			switch (code) {
-			case CREATE:
-				String user;
-				if((user = parameters.get("user"))== null){
-					handleError(new InternalBackEndException("missing user argument!"), response);
+				case CREATE :
+					String user;
+					if ((user = parameters.get("user")) == null) {
+						handleError(new InternalBackEndException("missing user argument!"), response);
+						return;
+					}
+
+					MUserBean userBean = null;
+					try {
+						userBean = getGson().fromJson(user, MUserBean.class);
+						userBean.setSocialNetworkID("MYMED");
+						userBean.setSocialNetworkName("myMed");
+					} catch (final JsonSyntaxException e) {
+						handleError(new InternalBackEndException("user jSon format is not valid"), response);
+						return;
+					}
+
+					MAuthenticationBean authenticationBean = null;
+					try {
+						authenticationBean = getGson().fromJson(authentication, MAuthenticationBean.class);
+					} catch (final JsonSyntaxException e) {
+						handleError(new InternalBackEndException("authentication jSon format is not valid"), response);
+						return;
+					}
+
+					MLogger.getLog().info("Trying to create a new user:\n {}", userBean.toString());
+
+					userBean = authenticationManager.create(userBean, authenticationBean);
+
+					MLogger.getLog().info("User created");
+
+					setResponseText(getGson().toJson(userBean));
+					break;
+				case UPDATE :
+					try {
+						authenticationBean = getGson().fromJson(authentication, MAuthenticationBean.class);
+					} catch (final JsonSyntaxException e) {
+						handleError(new InternalBackEndException("authentication jSon format is not valid"), response);
+						return;
+					}
+
+					String id;
+					if ((id = parameters.get("id")) == null) {
+						handleError(new InternalBackEndException("missing user argument!"), response);
+						return;
+					}
+
+					MLogger.getLog().info("Trying to update authentication:\n {}", authenticationBean.toString());
+
+					authenticationManager.update(id, authenticationBean);
+
+					MLogger.getLog().info("Authentication updated!");
+
+					break;
+				default :
+					handleError(new InternalBackEndException("ProfileRequestHandler.doPost(" + code + ") not exist!"),
+					        response);
 					return;
-				}
-				MUserBean userBean = null;
-				try {
-					userBean = getGson().fromJson(user,
-							MUserBean.class);
-					userBean.setSocialNetworkID("MYMED");
-					userBean.setSocialNetworkName("myMed");
-				} catch (JsonSyntaxException e) {
-					handleError(new InternalBackEndException("user jSon format is not valid"), response);
-					return;
-				}
-				MAuthenticationBean authenticationBean = null;
-				try {
-					authenticationBean = getGson().fromJson(authentication,
-							MAuthenticationBean.class);
-				} catch (JsonSyntaxException e) {
-					handleError(new InternalBackEndException("authentication jSon format is not valid"), response);
-					return;
-				}
-				System.out.println("\nINFO: trying to create a new user:\n" + userBean);
-				userBean = authenticationManager.create(userBean, authenticationBean);
-				System.out.println("\nINFO: User created!");
-				setResponseText(getGson().toJson(userBean));
-				break;
-			case UPDATE:
-				try {
-					authenticationBean = getGson().fromJson(authentication,
-							MAuthenticationBean.class);
-				} catch (JsonSyntaxException e) {
-					handleError(new InternalBackEndException("authentication jSon format is not valid"), response);
-					return;
-				}
-				String id;
-				if((id = parameters.get("id"))== null){
-					handleError(new InternalBackEndException("missing user argument!"), response);
-					return;
-				}
-				System.out.println("\nINFO: trying to update authentication:\n" + authenticationBean);
-				authenticationManager.update(id, authenticationBean);
-				System.out.println("\nINFO: authentication updated!");
-				break;
-			default:
-				handleError(new InternalBackEndException("ProfileRequestHandler.doPost(" + code + ") not exist!"), response);
-				return;
 			}
 			super.doPost(request, response);
-		} catch (InternalBackEndException e) {
-			e.printStackTrace();
+		} catch (final InternalBackEndException e) {
+			MLogger.getLog().info("Error in doPost operation");
+			MLogger.getDebugLog().debug("Error in doPost operation", e.getCause());
 			handleError(e, response);
 			return;
-		} catch (IOBackEndException e) {
-			e.printStackTrace();
+		} catch (final IOBackEndException e) {
+			MLogger.getLog().info("Error in doPost operation");
+			MLogger.getDebugLog().debug("Error in doPost operation", e.getCause());
 			handleError(e, response);
 		}
 	}
-
 }
--- backend/src/com/mymed/controller/core/requesthandler/ProfileRequestHandler.java
+++ backend/src/com/mymed/controller/core/requesthandler/ProfileRequestHandler.java
@@ -13,6 +13,7 @@ import com.mymed.controller.core.exception.IOBackEndException;
 import com.mymed.controller.core.exception.InternalBackEndException;
 import com.mymed.controller.core.manager.profile.ProfileManager;
 import com.mymed.model.data.user.MUserBean;
+import com.mymed.utils.MLogger;
 
 /**
  * Servlet implementation class UsersRequestHandler
@@ -29,14 +30,15 @@ public class ProfileRequestHandler extends AbstractRequestHandler {
 	/* Constructors */
 	/* --------------------------------------------------------- */
 	/**
-	 * @throws ServletException 
+	 * @throws ServletException
 	 * @see HttpServlet#HttpServlet()
 	 */
 	public ProfileRequestHandler() throws ServletException {
 		super();
+
 		try {
-			this.profileManager = new ProfileManager();
-		} catch (InternalBackEndException e) {
+			profileManager = new ProfileManager();
+		} catch (final InternalBackEndException e) {
 			throw new ServletException("ProfileManager is not accessible because: " + e.getMessage());
 		}
 	}
@@ -48,48 +50,53 @@ public class ProfileRequestHandler extends AbstractRequestHandler {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+	@Override
+	protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException,
+	        IOException {
 		try {
 			/** Get the parameters */
-			Map<String, String> parameters = getParameters(request);
+			final Map<String, String> parameters = getParameters(request);
 
 			/** Get the method code */
-			RequestCode code = requestCodeMap.get(parameters.get("code"));
+			final RequestCode code = requestCodeMap.get(parameters.get("code"));
 
 			/** handle the request */
 			String id;
-			if((id = parameters.get("id"))== null){
+			if ((id = parameters.get("id")) == null) {
 				handleError(new InternalBackEndException("missing id argument!"), response);
 				return;
 			}
+
 			switch (code) {
-			case READ:
-				MUserBean userBean;
-				try {
-					userBean = profileManager.read(id);
-					setResponseText(getGson().toJson(userBean));
-				} catch (IOBackEndException e) {
-					handleError(e, response);
+				case READ :
+					MUserBean userBean;
+					try {
+						userBean = profileManager.read(id);
+						setResponseText(getGson().toJson(userBean));
+					} catch (final IOBackEndException e) {
+						handleError(e, response);
+						return;
+					}
+					break;
+				case DELETE :
+					profileManager.delete(id);
+					MLogger.getLog().info("User '{}' deleted", id);
+					break;
+				default :
+					handleError(new InternalBackEndException("ProfileRequestHandler.doGet(" + code + ") not exist!"),
+					        response);
 					return;
-				}
-				break;
-			case DELETE:
-				profileManager.delete(id);
-				System.out.println("\nINFO: User " + id
-						+ " deleted!");
-				break;
-			default:
-				handleError(new InternalBackEndException("ProfileRequestHandler.doGet(" + code + ") not exist!"), response);
-				return;
 			}
+
 			super.doGet(request, response);
-		} catch (InternalBackEndException e) {
-			e.printStackTrace();
+		} catch (final InternalBackEndException e) {
+			MLogger.getLog().info("Error in doGet");
+			MLogger.getDebugLog().debug("Error in doGet", e.getCause());
 			handleError(e, response);
 			return;
-		} catch (IOBackEndException e) {
-			e.printStackTrace();
+		} catch (final IOBackEndException e) {
+			MLogger.getLog().info("Error in doGet");
+			MLogger.getDebugLog().debug("Error in doGet", e.getCause());
 			handleError(e, response);
 		}
 	}
@@ -98,61 +105,72 @@ public class ProfileRequestHandler extends AbstractRequestHandler {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+	@Override
+	protected void doPost(final HttpServletRequest request, final HttpServletResponse response)
+	        throws ServletException, IOException {
 		try {
 			/** Get the parameters */
-			Map<String, String> parameters = getParameters(request);
+			final Map<String, String> parameters = getParameters(request);
 
 			/** Get the method code */
-			RequestCode code = requestCodeMap.get(parameters.get("code"));
+			final RequestCode code = requestCodeMap.get(parameters.get("code"));
 
 			/** handle the request */
 			String user;
-			if((user = parameters.get("user"))== null){
+			if ((user = parameters.get("user")) == null) {
 				handleError(new InternalBackEndException("missing user argument!"), response);
 				return;
 			}
 			switch (code) {
-			case CREATE:
-				MUserBean userBean = null;
-				try {
-					System.out.println("USER:\n\n" + user);
-					userBean = getGson().fromJson(user,
-							MUserBean.class);
-				} catch (JsonSyntaxException e) {
-					handleError(new InternalBackEndException("user jSon format is not valid"), response);
+				case CREATE :
+					MUserBean userBean = null;
+					try {
+						MLogger.getLog().info("User:\n", user);
+						userBean = getGson().fromJson(user, MUserBean.class);
+					} catch (final JsonSyntaxException e) {
+						handleError(new InternalBackEndException("user jSon format is not valid"), response);
+						return;
+					}
+
+					MLogger.getLog().info("Trying to create a new user:\n {}", userBean.toString());
+
+					userBean = profileManager.create(userBean);
+
+					MLogger.getLog().info("User created!");
+
+					setResponseText(getGson().toJson(userBean));
+					break;
+				case UPDATE :
+					try {
+						userBean = getGson().fromJson(user, MUserBean.class);
+					} catch (final JsonSyntaxException e) {
+						handleError(new InternalBackEndException("user jSon format is not valid"), response);
+						return;
+					}
+
+					MLogger.getLog().info("Trying to update user:\n {}", userBean.toString());
+
+					userBean = profileManager.update(userBean);
+					setResponseText(getGson().toJson(userBean));
+
+					MLogger.getLog().info("User updated!");
+
+					break;
+				default :
+					handleError(new InternalBackEndException("ProfileRequestHandler.doPost(" + code + ") not exist!"),
+					        response);
 					return;
-				}
-				System.out.println("\nINFO: trying to create a new user:\n" + userBean);
-				userBean = profileManager.create(userBean);
-				System.out.println("\nINFO: User created!");
-				setResponseText(getGson().toJson(userBean));
-				break;
-			case UPDATE:
-				try {
-					userBean = getGson().fromJson(user,
-							MUserBean.class);
-				} catch (JsonSyntaxException e) {
-					handleError(new InternalBackEndException("user jSon format is not valid"), response);
-					return;
-				}
-				System.out.println("\nINFO: trying to update user:\n" + userBean);
-				userBean = profileManager.update(userBean);
-				setResponseText(getGson().toJson(userBean));
-				System.out.println("\nINFO: User updated!");
-				break;
-			default:
-				handleError(new InternalBackEndException("ProfileRequestHandler.doPost(" + code + ") not exist!"), response);
-				return;
 			}
+
 			super.doPost(request, response);
-		} catch (InternalBackEndException e) {
-			e.printStackTrace();
+		} catch (final InternalBackEndException e) {
+			MLogger.getLog().info("Error in doPost");
+			MLogger.getDebugLog().debug("Error in doPost", e.getCause());
 			handleError(e, response);
 			return;
-		} catch (IOBackEndException e) {
-			e.printStackTrace();
+		} catch (final IOBackEndException e) {
+			MLogger.getLog().info("Error in doPost");
+			MLogger.getDebugLog().debug("Error in doPost", e.getCause());
 			handleError(e, response);
 		}
 	}
