9dfa7cb7b94598184dd21d053415b7ffeb20fb82
Milo Casagrande <milo.casagrande@inria.fr>
2011-09-28 16:49:56 +0200

    Fixed ReputationManager test

--- backend/src/com/mymed/tests/unit/ReputationManagerTest.java
+++ backend/src/com/mymed/tests/unit/ReputationManagerTest.java
@@ -12,34 +12,47 @@ import com.mymed.model.data.reputation.MReputationBean;
  * Test class for the {@link ReputationManager}
  * 
  * @author Milo Casagrande
- * 
  */
 public class ReputationManagerTest extends GeneralTest {
 
-	private static final double FEEDBACK = 2;
+  private static final double FEEDBACK = 2;
 
-	/**
-	 * Perform an update operation
-	 */
-	@Test
-	public void testUpdateReputation() {
-		try {
-//			reputationManager.update(interactionBean, FEEDBACK);
-		} catch (final Exception ex) {
-			fail(ex.getMessage());
-		}
-	}
+  @Test
+  public void testCreateReputation() {
+    try {
+      reputationManager.create(reputationBean, APPLICATION_ID + PRODUCER_ID);
+    } catch (final Exception ex) {
+      fail(ex.getMessage());
+    }
+  }
 
-	/**
-	 * Read the reputation back from the database
-	 */
-	@Test
-	public void testReadReputation() {
-		try {
-			final MReputationBean beanRead = reputationManager.read(PRODUCER_ID, CONSUMER_ID, APPLICATION_ID);
-			assertEquals("The reputation beans are not the same\n", FEEDBACK, beanRead.getValue(), 0);
-		} catch (final Exception ex) {
-			fail(ex.getMessage());
-		}
-	}
+  /**
+   * Read the reputation back from the database
+   */
+  @Test
+  public void testReadReputation() {
+    try {
+      final MReputationBean readReputation = reputationManager.read(PRODUCER_ID, CONSUMER_ID, APPLICATION_ID);
+      assertEquals("The reputation beans are not the same\n", reputationBean, readReputation);
+    } catch (final Exception ex) {
+      fail(ex.getMessage());
+    }
+  }
+
+  /**
+   * Perform an update operation
+   */
+  @Test
+  public void testUpdateReputation() {
+    try {
+      final MReputationBean newReputationBean = reputationBean.clone();
+      newReputationBean.setValue(FEEDBACK);
+
+      reputationManager.update(newReputationBean, APPLICATION_ID + PRODUCER_ID);
+      final MReputationBean readReputation = reputationManager.read(PRODUCER_ID, CONSUMER_ID, APPLICATION_ID);
+      assertEquals("The reputation beans are not the same\n", reputationBean, readReputation);
+    } catch (final Exception ex) {
+      fail(ex.getMessage());
+    }
+  }
 }
