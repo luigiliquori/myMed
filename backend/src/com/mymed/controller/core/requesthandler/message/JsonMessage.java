f4f9ae57bffe1510abacc028ce7423b03481f951
laurent vanni <lvanni@mymed2.sophia.inria.fr>
2011-10-18 11:40:55 +0200

    NEW: 	- javadoc updated
    _LV

--- backend/src/com/mymed/controller/core/requesthandler/message/JsonMessage.java
+++ backend/src/com/mymed/controller/core/requesthandler/message/JsonMessage.java
@@ -12,6 +12,48 @@ import com.google.gson.Gson;
  */
 public class JsonMessage {
 
+	/* --------------------------------------------------------- */
+	/* Attributes */
+	/* --------------------------------------------------------- */
+	private int status;
+	private String handler;
+	private String method;
+	private String description;
+	private Map<String, String> data;
+
+	/* --------------------------------------------------------- */
+	/* Constructors */
+	/* --------------------------------------------------------- */
+	/**
+	 * Default Constructor
+	 * @param status 
+	 * 		server status: 200|404|500|..
+	 * @param handler
+	 * 		The ClassName of the handler responsible for the message
+	 */
+	public JsonMessage(int status, String handler) {
+		this(status, handler, "unknown", new HashMap<String, String>());
+	}
+	
+	/**
+	 * 
+	 * @param status
+	 * 		The server status: (see http server code 200|404|500|...)
+	 * @param handler
+	 * 		The ClassName of the handler responsible for the message
+	 * @param method
+	 * 		The method ID: CREATE|READ|UPDATE|DELETE
+	 * @param data
+	 * 		The content of the response
+	 */
+	public JsonMessage(int status, String handler, String method, Map<String, String> data) {
+		this.status = status;
+		this.handler = handler;
+		this.method = method;
+		this.data = data;
+	}
+	
+	
 	/*
 	 * message {
 	 * 		"status"		: 200|404|500|... ,
@@ -24,30 +66,15 @@ public class JsonMessage {
 	 * 		]
 	 * }
 	 */
-	
-	private int status;
-	private String handler;
-	private String method;
-	private String description;
-	private Map<String, String> data;
-
-	public JsonMessage(int status, String handler) {
-		this(status, handler, "unknown", new HashMap<String, String>());
-	}
-	
-	public JsonMessage(int status, String handler, String method,Map<String, String> data) {
-		this.status = status;
-		this.handler = handler;
-		this.method = method;
-		this.data = data;
-	}
-	
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
 		return new Gson().toJson(this);
 	}
 
+	/* --------------------------------------------------------- */
+	/* GETTER&SETTER */
+	/* --------------------------------------------------------- */
 	public int getStatus() {
 		return status;
 	}
