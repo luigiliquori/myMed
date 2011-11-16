3698423c7189e3880bad453e7133c3798313e3cd
Milo Casagrande <milo.casagrande@inria.fr>
2011-11-16 11:17:50 +0100

    Worked on the logger funcionalities
    	* In order to simplify the logger calls, deprecate the old
    	  calls, and introduce the necessary ones, without the need
    	  to get the logger first.

--- backend/src/com/mymed/utils/MLogger.java
+++ backend/src/com/mymed/utils/MLogger.java
@@ -12,71 +12,122 @@ import ch.qos.logback.classic.Logger;
  * 
  */
 public final class MLogger {
-	/*
-	 * The private instance for the MLogger class to implement the singleton
-	 */
-	private static final MLogger INSTANCE = new MLogger();
+  /*
+   * The private instance for the MLogger class to implement the singleton
+   */
+  private static final MLogger INSTANCE = new MLogger();
 
-	/*
-	 * The name of the default info logger as defined in the file logback.xml
-	 */
-	private static final String INFO_LOG_NAME = "mymed.info.logger";
+  /*
+   * The name of the default info logger as defined in the file logback.xml
+   */
+  private static final String INFO_LOG_NAME = "mymed.info.logger";
 
-	/*
-	 * The name of the default debug logger as defined in the file logback.xml
-	 */
-	private static final String DEBUG_LOG_NAME = "mymed.debug.logger";
+  /*
+   * The name of the default debug logger as defined in the file logback.xml
+   */
+  private static final String DEBUG_LOG_NAME = "mymed.debug.logger";
 
-	/*
-	 * The default info logger to be used throughout all the managers
-	 */
-	private static final Logger INFO_LOGGER = (Logger) LoggerFactory.getLogger(INFO_LOG_NAME);
+  /*
+   * The default info logger to be used throughout all the managers
+   */
+  private static final Logger INFO_LOGGER = (Logger) LoggerFactory.getLogger(INFO_LOG_NAME);
 
-	/*
-	 * The default debug logger to be used throughout all the managers
-	 */
-	private static final Logger DEBUG_LOGGER = (Logger) LoggerFactory.getLogger(DEBUG_LOG_NAME);
+  /*
+   * The default debug logger to be used throughout all the managers
+   */
+  private static final Logger DEBUG_LOGGER = (Logger) LoggerFactory.getLogger(DEBUG_LOG_NAME);
 
-	/*
-	 * Private constructor since all methods are static, or we have to provide a
-	 * singleton
-	 */
-	private MLogger() {
-	}
+  /*
+   * Private constructor since all methods are static and we have a singleton
+   */
+  private MLogger() {
+  }
 
-	/**
-	 * Retrieve the MLogger instance to be use as a singleto
-	 * 
-	 * @return the MLogger instance
-	 */
-	public static MLogger getInstance() {
-		return INSTANCE;
-	}
+  /**
+   * Retrieve the MLogger instance to be used as a singleton
+   * 
+   * @return the MLogger instance
+   */
+  public static MLogger getInstance() {
+    return INSTANCE;
+  }
 
-	/**
-	 * Get the default mymed logger, in this case an 'info' level logger
-	 * 
-	 * @return the default logger
-	 */
-	public static Logger getLog() {
-		return INFO_LOGGER;
-	}
+  /**
+   * Get the default mymed logger, in this case an 'info' level logger
+   * 
+   * @return the default logger
+   */
+  @Deprecated
+  public static Logger getLog() {
+    return INFO_LOGGER;
+  }
 
-	/**
-	 * Get the 'info' level logger of mymed
-	 * 
-	 * @return the info level logger
-	 */
-	public static Logger getInfoLog() {
-		return INFO_LOGGER;
-	}
+  /**
+   * Get the 'info' level logger of mymed
+   * 
+   * @return the info level logger
+   */
+  @Deprecated
+  public static Logger getInfoLog() {
+    return INFO_LOGGER;
+  }
 
-	/**
-	 * Get the 'debug' level logger of mymed
-	 * 
-	 * @return the debug level logger
-	 */
-	public static Logger getDebugLog() {
-		return DEBUG_LOGGER;
-	}
+  /**
+   * Get the 'debug' level logger of mymed
+   * 
+   * @return the debug level logger
+   */
+  @Deprecated
+  public static Logger getDebugLog() {
+    return DEBUG_LOGGER;
+  }
+
+  /**
+   * @see ch.qos.logback.classic.Logger#info(String)
+   * @param msg
+   *          the message to log
+   */
+  public static void info(final String msg) {
+    INFO_LOGGER.info(msg);
+  }
+
+  /**
+   * @see ch.qos.logback.classic.Logger#info(String, Object)
+   * @param format
+   *          the format of the string to log
+   * @param arg
+   *          that arguments used
+   */
+  public static void info(final String format, final Object arg) {
+    INFO_LOGGER.info(format, arg);
+  }
+
+  /**
+   * @see ch.qos.logback.classic.Logger#warn(String)
+   * @param msg
+   *          the message to log
+   */
+  public static void warn(final String msg) {
+    INFO_LOGGER.warn(msg);
+  }
+
+  /**
+   * @see ch.qos.logback.classic.Logger#debug(String)
+   * @param msg
+   *          the message to log
+   */
+  public static void debug(final String msg) {
+    DEBUG_LOGGER.debug(msg);
+  }
+
+  /**
+   * @see ch.qos.logback.classic.Logger#debug(String, Throwable)
+   * @param msg
+   *          the message to log
+   * @param cause
+   *          the potential exception
+   */
+  public static void debug(final String msg, final Throwable cause) {
+    DEBUG_LOGGER.debug(msg, cause);
+  }
 }
