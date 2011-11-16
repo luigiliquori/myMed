41e4208b310451b52e809c507706c7410d6fb1b1
Milo Casagrande <milo.casagrande@inria.fr>
2011-11-16 11:40:17 +0100

    Removed unused imports

--- backend/src/com/mymed/controller/core/exception/AbstractMymedException.java
+++ backend/src/com/mymed/controller/core/exception/AbstractMymedException.java
@@ -1,10 +1,5 @@
 package com.mymed.controller.core.exception;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import com.mymed.controller.core.requesthandler.message.JsonMessage;
-
 /**
  * An myMed exception is used to convert the Exception message in a jSon format
  * for the frontend
@@ -13,30 +8,31 @@ import com.mymed.controller.core.requesthandler.message.JsonMessage;
  */
 public abstract class AbstractMymedException extends Exception {
 
-	/* --------------------------------------------------------- */
-	/* Attributes */
-	/* --------------------------------------------------------- */
-	private static final long serialVersionUID = 1L;
-	/** the status returned by the http server */
-	private int status;
-	private String message;
+  /* --------------------------------------------------------- */
+  /* Attributes */
+  /* --------------------------------------------------------- */
+  private static final long serialVersionUID = 1L;
+  /** the status returned by the http server */
+  private final int status;
+  private final String message;
 
-	/* --------------------------------------------------------- */
-	/* Constructors */
-	/* --------------------------------------------------------- */
-	public AbstractMymedException(int status, final String message) {
-		this.status = status;
-		this.message = message;
-	}
+  /* --------------------------------------------------------- */
+  /* Constructors */
+  /* --------------------------------------------------------- */
+  public AbstractMymedException(final int status, final String message) {
+    this.status = status;
+    this.message = message;
+  }
 
-	/* --------------------------------------------------------- */
-	/* Public methods */
-	/* --------------------------------------------------------- */
-	public int getStatus(){
-		return this.status;
-	}
-	
-	public String getMessage(){
-		return this.message;
-	}
+  /* --------------------------------------------------------- */
+  /* Public methods */
+  /* --------------------------------------------------------- */
+  public int getStatus() {
+    return status;
+  }
+
+  @Override
+  public String getMessage() {
+    return message;
+  }
 }
