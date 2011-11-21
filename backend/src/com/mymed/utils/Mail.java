818a1f093a5fb23312a12ef5ff129240837490c8
Milo Casagrande <milo.casagrande@inria.fr>
2011-11-21 14:56:01 +0100

    Fixed logger use
    	* Use new logger calls

--- backend/src/com/mymed/utils/Mail.java
+++ backend/src/com/mymed/utils/Mail.java
@@ -12,47 +12,47 @@ import javax.mail.internet.MimeMessage;
 
 public class Mail {
 
-	private final Properties props;
-	private final String from, to, object, content;
+  private final Properties props;
+  private final String from, to, object, content;
 
-	public Mail(final String from, final String to, final String object, final String content) {
-		props = new Properties();
-		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.socketFactory.port", "465");
-		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.port", "465");
+  public Mail(final String from, final String to, final String object, final String content) {
+    props = new Properties();
+    props.put("mail.smtp.host", "smtp.gmail.com");
+    props.put("mail.smtp.socketFactory.port", "465");
+    props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+    props.put("mail.smtp.auth", "true");
+    props.put("mail.smtp.port", "465");
 
-		this.from = from;
-		this.to = to;
-		this.object = object;
-		this.content = content;
+    this.from = from;
+    this.to = to;
+    this.object = object;
+    this.content = content;
 
-		sendMail();
-	}
+    sendMail();
+  }
 
-	private void sendMail() {
-		final Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-			@Override
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication("mymed.subscribe", "myalcotra");
-			}
-		});
+  private void sendMail() {
+    final Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+      @Override
+      protected PasswordAuthentication getPasswordAuthentication() {
+        return new PasswordAuthentication("mymed.subscribe", "myalcotra");
+      }
+    });
 
-		try {
+    try {
 
-			final Message message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(from));
-			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
-			message.setSubject(object);
-			message.setText(content);
+      final Message message = new MimeMessage(session);
+      message.setFrom(new InternetAddress(from));
+      message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+      message.setSubject(object);
+      message.setText(content);
 
-			Transport.send(message);
+      Transport.send(message);
 
-			MLogger.getLog().info("Email sent!");
+      MLogger.info("Email sent!");
 
-		} catch (final MessagingException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    } catch (final MessagingException e) {
+      throw new RuntimeException(e);
+    }
+  }
 }
