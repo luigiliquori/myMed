7ef8948f0958bbdb4ca4dbf1a84db1d0055c2b69
Milo Casagrande <milo.casagrande@inria.fr>
2011-11-24 12:07:58 +0100

    Fixed problem with session manager tests

--- backend/src/com/mymed/tests/unit/manager/SessionManagerTest.java
+++ backend/src/com/mymed/tests/unit/manager/SessionManagerTest.java
@@ -4,10 +4,10 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import java.util.Map;
-
 import org.junit.Test;
 
+import com.mymed.controller.core.exception.IOBackEndException;
+import com.mymed.controller.core.exception.InternalBackEndException;
 import com.mymed.controller.core.manager.position.PositionManager;
 import com.mymed.model.data.session.MSessionBean;
 
@@ -31,7 +31,7 @@ public class SessionManagerTest extends GeneralTest {
   @Test
   public void testReadSession() {
     try {
-      final MSessionBean readSession = sessionManager.read(USER_ID);
+      final MSessionBean readSession = sessionManager.read(SESSION_ID);
       assertEquals("The sessions beans are not the same\n", sessionBean, readSession);
     } catch (final Exception ex) {
       fail(ex.getMessage());
@@ -44,21 +44,16 @@ public class SessionManagerTest extends GeneralTest {
       sessionBean.setP2P(true);
 
       sessionManager.update(sessionBean);
-      final MSessionBean readSession = sessionManager.read(USER_ID);
+      final MSessionBean readSession = sessionManager.read(SESSION_ID);
       assertTrue("The session bean has not been updated correctly", readSession.isP2P());
     } catch (final Exception ex) {
       fail(ex.getMessage());
     }
   }
 
-  @Test
-  public void testDeleteSession() {
-    try {
-      sessionManager.delete(USER_ID);
-      final Map<byte[], byte[]> column = storageManager.selectAll("Session", USER_ID + "_SESSION");
-      assertTrue("The number of columns after a delete is not 0", column.isEmpty());
-    } catch (final Exception ex) {
-      fail(ex.getMessage());
-    }
+  @Test(expected = IOBackEndException.class)
+  public void testDeleteSession() throws InternalBackEndException, IOBackEndException {
+    sessionManager.delete(SESSION_ID);
+    sessionManager.read(SESSION_ID);
   }
 }
