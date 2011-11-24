589a7dcdec446efdd45a65f4ae1836f7896a0a5e
Milo Casagrande <milo.casagrande@inria.fr>
2011-11-24 15:27:02 +0100

    Fixed equals() method

--- backend/src/com/mymed/model/data/session/MAuthenticationBean.java
+++ backend/src/com/mymed/model/data/session/MAuthenticationBean.java
@@ -12,122 +12,121 @@ import com.mymed.model.data.user.MUserBean;
  * 
  */
 public class MAuthenticationBean extends AbstractMBean {
+  /*
+   * Used for the calculation of the hashCode()
+   */
+  private static final int PRIME = 31;
 
-	/*
-	 * Used for the calculation of the hashCode()
-	 */
-	private static final int PRIME = 31;
+  /** AUTHENTICATION_ID */
+  private String login = "";
+  /** USER_ID */
+  private String user = "";
+  /** sha256(string) */
+  private String password = "";
 
-	/** AUTHENTICATION_ID */
-	private String login = "";
-	/** USER_ID */
-	private String user = "";
-	/** sha256(string) */
-	private String password = "";
+  @Override
+  public String toString() {
+    return "Autentication:\n" + super.toString();
+  }
 
-	@Override
-	public String toString() {
-		return "Autentication:\n" + super.toString();
-	}
+  /* --------------------------------------------------------- */
+  /* GETTER AND SETTER */
+  /* --------------------------------------------------------- */
+  /**
+   * @return the AUTHENTICATION_ID
+   */
+  public String getLogin() {
+    return login;
+  }
 
-	/* --------------------------------------------------------- */
-	/* GETTER AND SETTER */
-	/* --------------------------------------------------------- */
-	/**
-	 * @return the AUTHENTICATION_ID
-	 */
-	public String getLogin() {
-		return login;
-	}
+  /**
+   * @param login
+   *          the AUTHENTICATION_ID
+   */
+  public void setLogin(final String login) {
+    this.login = login;
+  }
 
-	/**
-	 * @param login
-	 *            the AUTHENTICATION_ID
-	 */
-	public void setLogin(final String login) {
-		this.login = login;
-	}
+  /**
+   * @return the ID of the authenticated user
+   */
+  public String getUser() {
+    return user;
+  }
 
-	/**
-	 * @return the ID of the authenticated user
-	 */
-	public String getUser() {
-		return user;
-	}
+  /**
+   * @param user
+   *          the ID of the user from {@link MUserBean}
+   */
+  public void setUser(final String user) {
+    this.user = user;
+  }
 
-	/**
-	 * @param user
-	 *            the ID of the user from {@link MUserBean}
-	 */
-	public void setUser(final String user) {
-		this.user = user;
-	}
+  /**
+   * @return the password SHA-256'ed
+   */
+  public String getPassword() {
+    return password;
+  }
 
-	/**
-	 * @return the password SHA-256'ed
-	 */
-	public String getPassword() {
-		return password;
-	}
+  /**
+   * @param password
+   *          the SHA-256 conversion of the password
+   */
+  public void setPassword(final String password) {
+    this.password = password;
+  }
 
-	/**
-	 * @param password
-	 *            the SHA-256 conversion of the password
-	 */
-	public void setPassword(final String password) {
-		this.password = password;
-	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see java.lang.Object#hashCode()
+   */
+  @Override
+  public int hashCode() {
+    int result = 1;
+    result = PRIME * result + (login == null ? 0 : login.hashCode());
+    result = PRIME * result + (password == null ? 0 : password.hashCode());
+    result = PRIME * result + (user == null ? 0 : user.hashCode());
+    return result;
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#hashCode()
-	 */
-	@Override
-	public int hashCode() {
-		int result = 1;
-		result = PRIME * result + (login == null ? 0 : login.hashCode());
-		result = PRIME * result + (password == null ? 0 : password.hashCode());
-		result = PRIME * result + (user == null ? 0 : user.hashCode());
-		return result;
-	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see java.lang.Object#equals(java.lang.Object)
+   */
+  @Override
+  public boolean equals(final Object object) {
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(final Object object) {
+    boolean equal = false;
 
-		boolean equal = false;
+    if (this == object) {
+      equal = true;
+    } else if (object instanceof MAuthenticationBean) {
+      final MAuthenticationBean comparable = (MAuthenticationBean) object;
 
-		if (this == object) {
-			equal = true;
-		} else if (object instanceof MAuthenticationBean) {
-			final MAuthenticationBean comparable = (MAuthenticationBean) object;
+      equal = true;
 
-			equal = true;
+      if (login == null && comparable.getLogin() != null) {
+        equal &= false;
+      } else if (login != null && comparable.getLogin() != null) {
+        equal &= login.equals(comparable.getLogin());
+      }
 
-			if (login == null && comparable.getLogin() != null) {
-				equal &= false;
-			} else {
-				equal &= login.equals(comparable.getLogin());
-			}
+      if (password == null && comparable.getPassword() != null) {
+        equal &= false;
+      } else if (password != null && comparable.getPassword() != null) {
+        equal &= password.equals(comparable.getPassword());
+      }
 
-			if (password == null && comparable.getPassword() != null) {
-				equal &= false;
-			} else {
-				equal &= password.equals(comparable.getPassword());
-			}
+      if (user == null && comparable.getUser() != null) {
+        equal &= false;
+      } else if (user != null && comparable.getUser() != null) {
+        equal &= user.equals(comparable.getUser());
+      }
+    }
 
-			if (user == null && comparable.getUser() != null) {
-				equal &= false;
-			} else {
-				equal &= user.equals(comparable.getUser());
-			}
-		}
-
-		return equal;
-	}
+    return equal;
+  }
 }
