b592be8f7f1663a02678db527cb0299b308e1be5
laurent vanni <lvanni@mymed2.sophia.inria.fr>
2012-01-06 15:42:54 +0100

    REFACOTRING: 	- FrontEnd: WebContent* and iGoogle* moved to old/ 	current folder created for the current version of the FrontEnd 	- FrontEnd: directories cleaned up => new folders lib/ system/ tools/ 	- First release of the d.a.s.p frontend library
    _LV

---
+++ frontend/www/current/lib/dasp/request/StartInteraction.class.php
@@ -0,0 +1,54 @@
+<?php 
+require_once 'lib/dasp/request/Request.class.php';
+require_once 'lib/dasp/beans/MDataBean.class.php';
+require_once 'system/templates/handler/IRequestHandler.php';
+
+/**
+ * 
+ * Request Handler for the tabBarMenu
+ * @author lvanni
+ *
+ */
+class StartInteraction extends Request {
+	/* --------------------------------------------------------- */
+	/* Attributes */
+	/* --------------------------------------------------------- */
+	private /*IRequestHandler*/ $handler;
+	
+	/* --------------------------------------------------------- */
+	/* Constructors */
+	/* --------------------------------------------------------- */
+	public function __construct(/*IRequestHandler*/ $handler) {
+		parent::__construct("InteractionRequestHandler", UPDATE);
+		$this->handler	= $handler;
+	}
+	
+	/* --------------------------------------------------------- */
+	/* Public methods */
+	/* --------------------------------------------------------- */
+	public /*void*/ function send() {
+		parent::addArgument("application", $_POST['application']);
+		parent::addArgument("producer", $_POST['producer']);
+		parent::addArgument("consumer", $_POST['consumer']);
+		parent::addArgument("start", $_POST['start']);
+		parent::addArgument("end", $_POST['end']);
+		parent::addArgument("predicate", $_POST['predicate']);
+		if(isset($_POST['snooze'])){
+			parent::addArgument("snooze", $_POST['snooze']);
+		}
+		// in case of "simple interaction"
+		if(isset($_POST['feedback'])){
+			parent::addArgument("feedback", $_POST['feedback']);
+		}
+		
+		$responsejSon = parent::send();
+		$responseObject = json_decode($responsejSon);
+		
+		if($responseObject->status != 200) {
+			$this->handler->setError($responseObject->description);
+		} else {
+			$this->handler->setSuccess($responseObject->description);
+		}
+	}
+}
+?>
