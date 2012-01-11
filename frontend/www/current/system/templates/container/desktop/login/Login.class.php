1b82729e3f6e33d2bfe9439f5238019ec450d430
laurent vanni <lvanni@mymed2.sophia.inria.fr>
2012-01-11 15:35:02 +0100

    NEW: 	- FrontEnd: refactoring of the css _LV

--- frontend/www/current/system/templates/container/desktop/login/Login.class.php
+++ frontend/www/current/system/templates/container/desktop/login/Login.class.php
@@ -23,7 +23,7 @@ class Login extends AbstractTemplate {
 	 * Default constructor
 	 */
 	public function __construct() {
-		parent::__construct("login");
+		parent::__construct("loginView", "loginView");
 		$this->handler = new InscriptionHandler();
 		$this->handler->handleRequest();
 	}
@@ -36,27 +36,26 @@ class Login extends AbstractTemplate {
 	*/
 	public /*String*/ function getHeader() { ?>
 		<!-- HEADER -->
-		<div align="center" style="position: absolute; top:0px; left: 0px; width: 100%; height: 100px; background-color: #2d2d2d;">
-			<div style="position: relative; width: 1024px;">
-				<img Style="position: absolute; top:17px; left: 0px;" alt="title" src="system/templates/container/desktop/login/img/title.png" height="30" />
+		<div id="loginHeader" align="center">
+			<div id="loginHeaderContainer">
+				<img id="logo" alt="title" src="system/templates/container/desktop/login/img/title.png" height="30" />
 				<form action="#" method="post" name="singinForm" id="singinForm">
 					<input type="hidden" name="singin" value="1"/>
 					<div data-role="fieldcontain">
-						<div style="position: absolute; left:380px; top:-10px; text-align: left;">
-							<label for="name" style="font-size: 9pt;">Adresse électronique:</label><br>
-						    <input type="text" name="login" id="login" value=""  Style="width: 200px;"/><br>
-						    <a href="#socialNetwork" data-rel="dialog" style="font-size: 9pt;">Connexion Via Réseau Social</a>
+						<div id="loginEmail">
+							<label for="name">Adresse électronique:</label><br>
+						    <input type="text" name="login" id="login" value="" size="45" data-theme="c"/><br>
+						    <a href="#socialNetwork" data-rel="dialog" >Connexion Via Réseau Social</a>
 					    </div>
-					    <div style="position: absolute; left:600px;  top:-10px; text-align: left;">
-						    <label for="name" style="font-size: 9pt;">Mot de passe:</label><br>
-						    <input type="password" name="password" id="password" value=""   Style="width: 200px;"/><br>
-						    <a href="#retrievePassword" style="font-size: 9pt;">Mot de passe oublié ?</a>
+					    <div id="loginPassword">
+						    <label for="name" >Mot de passe:</label><br>
+						    <input type="password" name="password" id="password" value="" size="45" data-theme="c"/><br>
+						    <a href="#retrievePassword" >Mot de passe oublié ?</a>
 					    </div>
-					    <div style="position: absolute; left:820px; top: 0px; text-align: left;">
-						    <a data-role="button" data-inline="true" onclick="document.singinForm.submit()" data-theme="a">Connexion</a>
+					    <div id="loginConnexion">
+						    <a data-role="button" data-inline="true" onclick="document.singinForm.submit()">Connexion</a>
 					    </div>
 					</div>	
-					<br />
 				</form>
 			</div>
 		</div>
@@ -68,20 +67,22 @@ class Login extends AbstractTemplate {
 	public /*String*/ function getContent() {
 		?>
 		<!-- CONTENT -->
-		<div  align="center" Style="position: absolute; top: 100px; left:0px; width: 100%; height:470px; center; background-image: url('system/templates/container/desktop/login/img/background.png');">
-			<div style="position: relative; width: 1024px;">
+		<div id="loginContent" align="center">
+			<div id="loginContainer">
 				<div class="ui-grid-a">
+					
 					<div class="ui-block-a" Style="text-align: left;">
-						<h3  style="color: black;">Profitez d'une plateforme extensible, <br> programmable et ouverte à tous</h3>
+						<h3>Profitez d'une plateforme extensible, <br> programmable et ouverte à tous</h3>
 						<img src="system/templates/container/desktop/login/img/devices.png" height="320" /><br>
-						<div style="position: relative; font-size: 9pt; color: black; top:20px;">
+						<div>
 							L'application myMed est bientôt disponible sur iPhone, iPad, Android, et plus encore...
 						</div>
 					</div>
+					
 					<div class="ui-block-b">
 						<form action="#" method="post" name="inscriptionForm" id="inscriptionForm">
-							<div data-role="fieldcontain" style="text-align: left; font-size: 9pt; color: black;">
-								<h3  style="color: black;">Inscription:</h3>
+							<div data-role="fieldcontain" >
+								<h3 >Inscription:</h3>
 								<!-- NOTIFICATION -->
 								<?php if($this->handler->getError()) { ?>
 									<div id="loginError" style="position: absolute; left:100px; top:0px; color: red;">
@@ -98,20 +99,25 @@ class Login extends AbstractTemplate {
 									</div>
 								<?php } ?>
 								<hr>
-								<input type="hidden" name="inscription" value="1" />
-								<span>Prénom : </span><br /><input type="text" name="prenom" /><br />
-								<span>Nom : </span><br /><input type="text" name="nom" /><br />
-								<span>eMail : </span><br /><input type="text" name="email" /><br />
-								<span>Password : </span><br /><input type="password" name="password" /><br />
-								<span>Confirm : </span><br /><input type="password" name="confirm" /><br />
-								<span>Date de naissance : </span><br /><input type="text" name="birthday" /><br />
-								<input type="checkbox" name="checkCondition" style="position: absolute; top: 13px;"/>
-								<span style="position: relative; left: 50px;">J'accepte les <a href="#">conditions d'utilisation</a></span><br />
+								<div data-role="fieldcontain">
+									<input type="hidden" name="inscription" value="1" />
+									<label>Prénom : </label><br /><input type="text" name="prenom" data-theme="d"/><br>
+									<label>Nom : </label><br /><input type="text" name="nom" data-theme="d"/><br>
+									<label>eMail : </label><br /><input type="text" name="email" data-theme="d"/><br>
+									<label>Password : </label><br /><input type="password" name="password" data-theme="d"/><br>
+									<label>Confirm : </label><br /><input type="password" name="confirm" data-theme="d"/><br>
+									<label>Date de naissance : </label><br /><input type="text" name="birthday" data-theme="d"/>
+									<input type="checkbox" name="checkCondition" style="position: absolute; top: 25px;"/><br>
+									<span style="position: relative; left: 50px;">J'accepte les <a href="#">conditions d'utilisation</a></span>
+								</div>
 								<hr>
-								<a Style="position: relative; left: 100px;" href="#" data-role="button" data-inline="true" onclick="window.document.getElementById('inscriptionForm').submit()">Création</a>
+								<div align="center">
+									<a href="#" data-role="button" data-inline="true" onclick="window.document.getElementById('inscriptionForm').submit()">Création</a>
+								</div>
 							</div>
 						</form>
 					</div>
+					
 				</div>
 			</div>
 		</div>
@@ -122,12 +128,12 @@ class Login extends AbstractTemplate {
 	*/
 	public /*String*/ function getFooter() { ?>
 		<!-- FOOTER -->
-		<div align="center" Style="position: absolute; top: 570px; left:0px; width: 100%; height:50px;">
-			<div style="position: relative; width: 1024px; left:-40px; font-size: 9pt;">
+		<div id="loginFooter" align="center">
+			<div id="loginFooterContainer">
 				<div style="position: absolute; left:0px;">
-					<a href="#" style="font-size: 9pt;">Francais</a>
-					<a href="#" style="font-size: 9pt;">English</a>
-					<a href="#" style="font-size: 9pt;">Italiano</a>
+					<a href="#" >Francais</a>
+					<a href="#" >English</a>
+					<a href="#" >Italiano</a>
 				</div>
 				<hr style="position: absolute; top:15px; width: 100%;">
 				<div style="position: absolute; top:30px; left:0px;">
