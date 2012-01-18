7ad5f8c86c0cf07bdf93e072386f62e81c0bb446
Milo Casagrande <milo.casagrande@inria.fr>
2012-01-18 11:03:54 +0100

    Fixed ant build script for website
    	* We were parsing also binary files!

--- frontend/www/current/system/config.php
+++ frontend/www/current/system/config.php
@@ -10,7 +10,7 @@
 	define('ROOTPATH', '/mymed/mobile/');
 	/** define the backend's URL'*/
 	define('MOBILE_PARAMETER_SEPARATOR', '::');
-	define('BACKEND_URL', 'http://mymed2.sophia.inria.fr:8080/mymed_backend/');
+	define('BACKEND_URL', '@backendurl@');
 	
 	//Social Networks Keys
 	define('Facebook_APP_ID', '263064853727560');
