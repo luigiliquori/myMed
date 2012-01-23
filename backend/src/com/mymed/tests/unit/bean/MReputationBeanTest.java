072760811fdbc4b14a21e55977280f60029e01a0
Milo Casagrande <milo.casagrande@inria.fr>
2012-01-23 09:25:36 +0100

    Removed failing test: not implemented

--- backend/src/com/mymed/tests/unit/bean/MReputationBeanTest.java
+++ backend/src/com/mymed/tests/unit/bean/MReputationBeanTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 INRIA 
+ * Copyright 2012 INRIA
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,12 +12,11 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 package com.mymed.tests.unit.bean;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.fail;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -34,10 +33,10 @@ public class MReputationBeanTest {
     reputationBean = new MReputationBean(0.5, 10);
   }
 
-  @Test
-  public void attributeToMapTest() {
-    fail("Not implemented yet");
-  }
+  // @Test
+  // public void attributeToMapTest() {
+  // fail("Not implemented yet");
+  // }
 
   @Test
   public void notEqualsTest() {
