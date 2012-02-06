bb93ff303c6e24443993686d8ce59231513ea61c
Milo Casagrande <milo.casagrande@inria.fr>
2012-02-06 16:39:15 +0100

    Added new bean unit test
    	* Added new unit test for the MOntologyBean
    	* Reworked the MOntologyBean in order to address problems
    	* Added new class to test suite

--- backend/src/com/mymed/model/data/interaction/MInteractionBean.java
+++ backend/src/com/mymed/model/data/interaction/MInteractionBean.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 INRIA 
+ * Copyright 2012 INRIA
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 package com.mymed.model.data.interaction;
 
 import com.mymed.model.data.AbstractMBean;
@@ -23,11 +23,6 @@ import com.mymed.model.data.AbstractMBean;
  * 
  */
 public class MInteractionBean extends AbstractMBean {
-  /**
-   * Used for the calculation of the hash code
-   */
-  private static final int PRIME = 31;
-
   /** INTERACTION_ID */
   private String id;
   /** APPLICATION_ID */
--- backend/src/com/mymed/tests/unit/bean/BeansTestSuite.java
+++ backend/src/com/mymed/tests/unit/bean/BeansTestSuite.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 INRIA 
+ * Copyright 2012 INRIA
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 package com.mymed.tests.unit.bean;
 
 import org.junit.runner.RunWith;
@@ -20,7 +20,8 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({MUserBeanTest.class, MSessionBeanTest.class, MInteractionBeanTest.class, MReputationBeanTest.class})
+@SuiteClasses({MUserBeanTest.class, MSessionBeanTest.class, MInteractionBeanTest.class, MReputationBeanTest.class,
+    MOntologyBeanTest.class})
 public class BeansTestSuite {
 
 }
