5efb22283ab943b480098e0be0fc60bb39de1841
Milo Casagrande <milo.casagrande@inria.fr>
2012-02-08 14:44:04 +0100

    Added MSearchBean test

--- backend/src/com/mymed/tests/unit/manager/AuthenticationManagerTest.java
+++ backend/src/com/mymed/tests/unit/manager/AuthenticationManagerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 INRIA 
+ * Copyright 2012 INRIA
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 package com.mymed.tests.unit.manager;
 
 import static org.junit.Assert.assertEquals;
@@ -31,30 +31,31 @@ import com.mymed.model.data.user.MUserBean;
  */
 public class AuthenticationManagerTest extends GeneralTest {
 
-	/**
-	 * Create a simple Authentication entry in the database
-	 */
-	@Test
-	public void createAuthTest() {
-		try {
-			authenticationManager.create(userBean, authenticationBean);
-		} catch (final Exception ex) {
-			fail(ex.getMessage());
-		}
-	}
+  /**
+   * Create a simple Authentication entry in the database
+   */
+  @Test
+  public void createAuthTest() {
+    try {
+      authenticationManager.create(userBean, authenticationBean);
+    } catch (final Exception ex) {
+      fail(ex.getMessage());
+    }
+  }
 
-	/**
-	 * Read the just created authentication entry from the database, and compare
-	 * the {@link MUserBean} returned
-	 */
-	@Test
-	public void readAuthTest() {
-		try {
-			final MUserBean readValue = authenticationManager.read(authenticationBean.getLogin(),
-			        authenticationBean.getPassword());
-			assertEquals("The user beans are not the same\n", readValue, userBean);
-		} catch (final Exception ex) {
-			fail(ex.getMessage());
-		}
-	}
+  /**
+   * Read the just created authentication entry from the database, and compare
+   * the {@link MUserBean} returned
+   */
+  @Test
+  public void readAuthTest() {
+    try {
+      authenticationManager.create(userBean, authenticationBean);
+      final MUserBean readValue = authenticationManager.read(authenticationBean.getLogin(),
+          authenticationBean.getPassword());
+      assertEquals("The user beans are not the same\n", readValue, userBean);
+    } catch (final Exception ex) {
+      fail(ex.getMessage());
+    }
+  }
 }
