d15195a86c5e08c0f442df2ded81f9bf800d7674
Milo Casagrande <milo.casagrande@inria.fr>
2012-02-08 15:14:12 +0100

    Added null test

--- backend/src/com/mymed/tests/unit/bean/MUserBeanTest.java
+++ backend/src/com/mymed/tests/unit/bean/MUserBeanTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 INRIA 
+ * Copyright 2012 INRIA
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
-*/
+ */
 package com.mymed.tests.unit.bean;
 
 import static org.junit.Assert.assertEquals;
@@ -49,9 +49,6 @@ public class MUserBeanTest {
   public void attributeToMapTest() {
     try {
       final Map<String, byte[]> attributesMap = userBean.getAttributeToMap();
-      // TODO needs to find a better way to structure this, we need to create
-      // enums for each beans and check that the
-      // fields are valid through that
       assertEquals(20, attributesMap.size());
     } catch (final Exception ex) {
       fail(ex.getMessage());
@@ -63,6 +60,16 @@ public class MUserBeanTest {
     final MUserBean actual = new MUserBean();
     actual.setId("ANOTHER_ID");
     actual.setName("ANOTHER_NAME");
+
+    assertFalse("The user beans are the same", userBean.equals(actual));
+  }
+
+  /**
+   * Test equals with null object
+   */
+  @Test
+  public void nullEqualsTest() {
+    final MUserBean actual = null;
 
     assertFalse("The user beans are the same", userBean.equals(actual));
   }
