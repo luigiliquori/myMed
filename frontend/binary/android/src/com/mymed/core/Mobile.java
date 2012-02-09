56baa935a7a92efb9d3ea6535700e4b5aa6b26be
laurent vanni <laurent.vanni@inria.fr>
2012-02-09 15:54:57 +0100

    NEW: 	- ChromeClient added into android binary BUG: 	- splash screen crash the application _LV

--- frontend/binary/android/src/com/mymed/core/Mobile.java
+++ frontend/binary/android/src/com/mymed/core/Mobile.java
@@ -13,13 +13,15 @@ public class Mobile extends Activity {
 
 	private WebView webView;
 	private WebClient webClient;
+	private ChromeWebClient chromeWebClient;
 
 	public static final String TAG = "*********MyMed";
 	public static final String MYMED_FRONTEND_URL = "http://mymed2.sophia.inria.fr";
 	public static final String MYMED_BACKEND_URL = "http://mymed2.sophia.inria.fr:8080/backend";
 
 	public Mobile() {
-		this.webClient = new WebClient(this);	
+		this.webClient = new WebClient(this);
+		this.chromeWebClient = new ChromeWebClient(this);
 	}
 
 	/** Called when the activity is first created. */
@@ -28,12 +30,14 @@ public class Mobile extends Activity {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.main);
-
-		// DEFAULT SETTINGS FOR THE WEBVIEW
+		
+		// GET WEB VIEW
 		webView = (WebView) findViewById(R.id.web_engine);
+		setContentView(webView);
 
-		// disable the toolbar
-		webView.setWebViewClient(webClient); 
+		// SET CLIENT
+		webView.setWebViewClient(webClient);
+		webView.setWebChromeClient(chromeWebClient);
 
 		// setup the zoom
 		webView.getSettings().setDefaultZoom(WebSettings.ZoomDensity.MEDIUM);
@@ -42,10 +46,39 @@ public class Mobile extends Activity {
 		webView.setVerticalScrollBarEnabled(false);
 		webView.setHorizontalScrollBarEnabled(false);
 
-		// enable javascript
-		webView.getSettings().setJavaScriptEnabled(true);
+		// set settings
+		WebSettings webSettings = webView.getSettings();
+		webSettings.setSavePassword(true);
+		webSettings.setSaveFormData(true);
+		webSettings.setJavaScriptEnabled(true);
+		webSettings.setSupportZoom(false);
+		webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+		webSettings.setGeolocationEnabled(true);
+		setContentView(R.layout.main);
+		
+		// load myMed URL
+		webView.loadUrl(MYMED_FRONTEND_URL);
+		
+		// launch the splash screen
+//		setContentView(R.layout.splash);
+//		Thread splashThread = new Thread() {
+//			@Override
+//			public void run() {
+//				try {
+//					int waited = 0;
+//					while (waited < 800) {
+//						sleep(400);
+//						waited += 100;
+//					}
+//				} catch (InterruptedException e) {
+//				} finally {
+					// set Clients
 
-		webView.loadUrl(MYMED_FRONTEND_URL); 
+//				}
+//			}
+//		};
+//
+//		splashThread.start();
 	}
 
 	public WebView getWebView() {
