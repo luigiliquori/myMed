1923f6f42f5932c9c25d320b998fae27cc9bb38b
cyril auburtin <cyril.auburtin@inria.fr>
2012-02-28 14:58:44 +0100

    FIX 	code cleanup

--- frontend/www/current/system/templates/application/myRiviera/views/tabbar/FindView.class.php
+++ frontend/www/current/system/templates/application/myRiviera/views/tabbar/FindView.class.php
@@ -39,9 +39,128 @@ class FindView extends MyApplication {
 	<h1>
 	<?= TARGET == "mobile" ? " " : APPLICATION_NAME ?>
 	</h1>
-	<a id="Iti" data-role="actionsheet" data-sheet="Itin"
-		data-icon="search" data-iconpos="right" class="ui-btn-left">Rechercher</a>
+
 	<a href="#Option" data-role="actionsheet" class="ui-btn-right" data-icon="gear">Options</a>
+	
+	<!-- ITINERAIRE POPUP -->
+	<a data-role="actionsheet"	data-icon="search" data-iconpos="right" class="ui-btn-left">Rechercher</a>
+	
+	<div data-theme="c">
+		<form action="#" method="post" name="<?= APPLICATION_NAME ?>FindForm"
+			id="<?= APPLICATION_NAME ?>FindForm">
+			<input type="hidden" name="application"
+				value="<?= APPLICATION_NAME ?>" /> <input type="hidden" name="method"
+				value="find" /> <input type="hidden" name="numberOfOntology"
+				value="4" /> <input type="hidden" id="departGeo" name="DepartGeo" />
+	
+			<!-- FROM -->
+	
+			<input data-theme="d" type="text" id="depart" name="Depart"
+				placeholder="Départ" />
+	
+			<!-- TO -->
+	
+			<div id="divarrivee">
+				<input data-theme="d" type="text" id="arrivee" name="Arrivee"
+					placeholder="Arrivée"
+					onkeyup="$(this).css('background-image', 'none');" /> <select
+					id="selectarrivee" data-iconpos="notext" data-icon="plus"
+					name="enum" onclick="changeDestination()">
+	
+					<!-- USER -->				
+			<?php if (isset($_SESSION['position'])) { ?>
+									<option value="<?= $_SESSION['user']->profilePicture ?>&&<?= $_SESSION['position']->formattedAddress ?>"><?= $_SESSION['user']->name ?></option>
+			<?php } ?>
+						
+							<!-- FRIENDS -->
+			<?php if(isset($_SESSION['friends'])) {
+							foreach ($_SESSION['friends'] as $friend ) { ?>
+			<?php if ($friend["position"]->formattedAddress != "") {?>
+									<option value="<?= $friend["profilePicture"] ?>&&<?= $friend["position"]->formattedAddress ?>">
+								<?= $friend["name"] ?>
+									</option>
+			<?php }
+					}
+				} ?>
+							  </select>
+			</div>
+	
+			<!-- DATE -->
+			<div id="date">
+	
+			<?php
+			$now = getdate();
+			$months = array('janvier', 'février', 'mars', 'avril','mai',
+					'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre');
+			?>
+				<fieldset data-role="controlgroup" data-type="horizontal" style="display: inline; margin: 5px;">
+	
+					<select name="select-day" id="select-day">
+					<?php for ($i = 1; $i <= 31; $i++) {
+						?>
+						<option value=<?=$i?> 
+						<?php if ($i==$now['mday']){?>
+							selected="selected"           <?php } ?>>
+							&nbsp;
+							<?= $i?>
+						</option>
+						<?php } ?>
+					</select> 
+					<select name="select-month" id="select-month">
+						<?php for ($i = 0; $i <= 11; $i++) {?>
+						<option value=<?=$i+1?>           
+						<?php if ($i+1==$now['mon']){?>
+							selected="selected" <?php } ?>>
+							<?=$months[$i]?>
+						</option>
+	
+							<?php } ?>
+						</select>
+						<select name="select-year" id="select-year">
+							<?php for ($i = 2012; $i <= 2016; $i++) {
+										?>
+							<option value=<?=$i?>
+							<?php if ($i==$now['year']){?>
+								selected="selected" <?php } ?>>
+								<?=$i?>
+							</option>
+						
+						<?php } ?>
+						</select>
+				</fieldset>
+	
+				<fieldset data-role="controlgroup" data-type="horizontal">
+					<select name="select-hour" id="select-hour">
+					<?php for ($i = 0; $i <= 23; $i++) {
+						?>
+						<option value=<?=$i?> <?php if ($i==$now['hours']){?>
+							selected="selected"   <?php } ?>>
+							<?=sprintf('%02d',$i)?>h
+						</option>
+											
+										<?php } ?>
+					</select>
+					<select name="select-minute" id="select-minute">
+									<?php for ($i = 0; $i <= 59; $i++) {
+										?>
+						<option value=<?=$i?> <?php if ($i==$now['minutes']){?>
+							selected="selected" <?php } ?>>
+							<?=sprintf('%02d',$i)?>
+						</option>
+							<?php } ?>
+					</select>
+				</fieldset>
+			</div>
+	
+			<!-- SUBMIT - ToDO validate before submit-->
+			<a href="#" id="trouver" data-role="button" rel="external"
+				data-icon="arrow-r" data-iconpos="right" data-theme="b"
+				onclick="validateIt();">Trouver</a>
+	
+		</form>
+	
+	</div>
+	
 </div>
 
 
@@ -74,128 +193,6 @@ public /*String*/ function getContent() { ?>
 		onclick=""></a>
 
 </div>
-
-
-
-
-<!-- ITINERAIRE POPUP -->
-
-<div id="Itin" data-theme="c">
-	<form action="#" method="post" name="<?= APPLICATION_NAME ?>FindForm"
-		id="<?= APPLICATION_NAME ?>FindForm">
-		<input type="hidden" name="application"
-			value="<?= APPLICATION_NAME ?>" /> <input type="hidden" name="method"
-			value="find" /> <input type="hidden" name="numberOfOntology"
-			value="4" /> <input type="hidden" id="departGeo" name="DepartGeo" />
-
-		<!-- FROM -->
-
-		<input data-theme="d" type="text" id="depart" name="Depart"
-			placeholder="Départ" />
-
-		<!-- TO -->
-
-		<div id="divarrivee">
-			<input data-theme="d" type="text" id="arrivee" name="Arrivee"
-				placeholder="Arrivée"
-				onkeyup="$(this).css('background-image', 'none');" /> <select
-				id="selectarrivee" data-iconpos="notext" data-icon="plus"
-				name="enum" onclick="changeDestination()">
-
-				<!-- USER -->				
-		<?php if (isset($_SESSION['position'])) { ?>
-								<option value="<?= $_SESSION['user']->profilePicture ?>&&<?= $_SESSION['position']->formattedAddress ?>"><?= $_SESSION['user']->name ?></option>
-		<?php } ?>
-					
-						<!-- FRIENDS -->
-		<?php if(isset($_SESSION['friends'])) {
-						foreach ($_SESSION['friends'] as $friend ) { ?>
-		<?php if ($friend["position"]->formattedAddress != "") {?>
-								<option value="<?= $friend["profilePicture"] ?>&&<?= $friend["position"]->formattedAddress ?>">
-							<?= $friend["name"] ?>
-								</option>
-		<?php }
-				}
-			} ?>
-						  </select>
-		</div>
-
-		<!-- DATE -->
-		<div id="date">
-
-		<?php
-		$now = getdate();
-		$months = array('janvier', 'février', 'mars', 'avril','mai',
-				'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre');
-		?>
-			<fieldset data-role="controlgroup" data-type="horizontal">
-
-				<select name="select-day" id="select-day">
-				<?php for ($i = 1; $i <= 31; $i++) {
-					?>
-					<option value=<?=$i?> 
-					<?php if ($i==$now['mday']){?>
-						selected="selected"           <?php } ?>>
-						&nbsp;
-						<?= $i?>
-					</option>
-					<?php } ?>
-				</select> 
-				<select name="select-month" id="select-month">
-					<?php for ($i = 0; $i <= 11; $i++) {?>
-					<option value=<?=$i+1?>           
-					<?php if ($i+1==$now['mon']){?>
-						selected="selected" <?php } ?>>
-						<?=$months[$i]?>
-					</option>
-
-						<?php } ?>
-					</select>
-					<select name="select-year" id="select-year">
-						<?php for ($i = 2012; $i <= 2016; $i++) {
-									?>
-						<option value=<?=$i?>
-						<?php if ($i==$now['year']){?>
-							selected="selected" <?php } ?>>
-							<?=$i?>
-						</option>
-					
-					<?php } ?>
-					</select>
-			</fieldset>
-
-			<fieldset data-role="controlgroup" data-type="horizontal">
-				<select name="select-hour" id="select-hour">
-				<?php for ($i = 0; $i <= 23; $i++) {
-					?>
-					<option value=<?=$i?> <?php if ($i==$now['hours']){?>
-						selected="selected"   <?php } ?>>
-						<?=sprintf('%02d',$i)?>h
-					</option>
-										
-									<?php } ?>
-				</select>
-				<select name="select-minute" id="select-minute">
-								<?php for ($i = 0; $i <= 59; $i++) {
-									?>
-					<option value=<?=$i?> <?php if ($i==$now['minutes']){?>
-						selected="selected" <?php } ?>>
-						<?=sprintf('%02d',$i)?>
-					</option>
-						<?php } ?>
-				</select>
-			</fieldset>
-		</div>
-
-		<!-- SUBMIT - ToDO validate before submit-->
-		<a href="#" id="trouver" data-role="button" rel="external"
-			data-icon="arrow-r" data-iconpos="right" data-theme="b"
-			onclick="validateIt();">Trouver</a>
-
-	</form>
-
-</div>
-
 
 <?php }
 
