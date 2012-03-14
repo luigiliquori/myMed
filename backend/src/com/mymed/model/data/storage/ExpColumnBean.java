ee42b055fce96a10f6b6c468578a06ae381c7f04
Milo Casagrande <milo.casagrande@inria.fr>
2012-03-14 15:01:32 +0100

    [backend] Added license header to POLITO files
    	* Aded Apache license header to all POLITO files
    	* Updated license header to two INRIA files

--- backend/src/com/mymed/model/data/storage/ExpColumnBean.java
+++ backend/src/com/mymed/model/data/storage/ExpColumnBean.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012 POLITO
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.mymed.model.data.storage;
 
 import java.util.Arrays;
@@ -6,74 +21,71 @@ import java.util.Arrays;
  * Bean that stores an expiring column.
  * 
  * @author iacopo
- * 
  */
 public class ExpColumnBean {
-  private byte[] value = new byte[0];
-  private long timestamp;
-  private int timeToLive;
+    private byte[] value = new byte[0];
+    private long timestamp;
+    private int timeToLive;
 
-  public void setValue(final byte[] value) {
-    this.value = Arrays.copyOf(value, value.length);
-  }
-
-  public byte[] getValue() {
-    return Arrays.copyOf(value, value.length);
-  }
-
-  public void setTimestamp(final long timestamp) {
-    this.timestamp = timestamp;
-  }
-
-  public long getTimestamp() {
-    return timestamp;
-  }
-
-  public void setTimeToLive(final int timeToLive) {
-    this.timeToLive = timeToLive;
-  }
-
-  public int getTimeToLive() {
-    return timeToLive;
-  }
-
-  /*
-   * (non-Javadoc)
-   * 
-   * @see java.lang.Object#hashCode()
-   */
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + timeToLive;
-    result = prime * result + (int) (timestamp ^ timestamp >>> 32);
-    result = prime * result + Arrays.hashCode(value);
-    return result;
-  }
-
-  /*
-   * (non-Javadoc)
-   * 
-   * @see java.lang.Object#equals(java.lang.Object)
-   */
-  @Override
-  public boolean equals(final Object obj) {
-
-    boolean equal = false;
-
-    if (this == obj) {
-      equal = true;
-    } else if (obj instanceof ExpColumnBean) {
-      final ExpColumnBean comparable = (ExpColumnBean) obj;
-
-      equal = true;
-
-      equal &= timeToLive == comparable.getTimeToLive();
-      equal &= timestamp == comparable.getTimestamp();
-      equal &= Arrays.equals(value, comparable.getValue());
+    public void setValue(final byte[] value) {
+        this.value = Arrays.copyOf(value, value.length);
     }
 
-    return equal;
-  }
+    public byte[] getValue() {
+        return Arrays.copyOf(value, value.length);
+    }
+
+    public void setTimestamp(final long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimeToLive(final int timeToLive) {
+        this.timeToLive = timeToLive;
+    }
+
+    public int getTimeToLive() {
+        return timeToLive;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = (prime * result) + timeToLive;
+        result = (prime * result) + (int) (timestamp ^ (timestamp >>> 32));
+        result = (prime * result) + Arrays.hashCode(value);
+        return result;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(final Object obj) {
+
+        boolean equal = false;
+
+        if (this == obj) {
+            equal = true;
+        } else if (obj instanceof ExpColumnBean) {
+            final ExpColumnBean comparable = (ExpColumnBean) obj;
+
+            equal = true;
+
+            equal &= timeToLive == comparable.getTimeToLive();
+            equal &= timestamp == comparable.getTimestamp();
+            equal &= Arrays.equals(value, comparable.getValue());
+        }
+
+        return equal;
+    }
 }
--- backend/src/com/mymed/model/data/user/MUserBean.java
+++ backend/src/com/mymed/model/data/user/MUserBean.java
@@ -1,12 +1,17 @@
 /*
- * Copyright 2012 INRIA Licensed under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance with the License.
+ * Copyright 2012 INRIA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
- * or agreed to in writing, software distributed under the License is
- * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.mymed.model.data.user;
 
@@ -129,9 +134,8 @@ public final class MUserBean extends AbstractMBean {
             final MUserBean comparable = (MUserBean) object;
 
             /*
-             * We compare only a subsets of the field to check that two
-             * MUserBean objects are the same. These should be values that are
-             * set for sure, and not null.
+             * We compare only a subsets of the field to check that two MUserBean objects are the same. These should be
+             * values that are set for sure, and not null.
              */
             equal = true;
 
