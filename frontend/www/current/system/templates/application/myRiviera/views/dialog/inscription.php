c6498436fce069462ad1db4b6633f293e97f7ede
laurent vanni <lvanni@mymed2.sophia.inria.fr>
2012-04-03 12:06:05 +0200

    NEW: myRiviera 	. login page updated _LV

--- frontend/www/current/system/templates/application/myRiviera/views/dialog/inscription.php
+++ frontend/www/current/system/templates/application/myRiviera/views/dialog/inscription.php
@@ -7,15 +7,19 @@
 
 	<!-- CONTENT -->
 	<div data-theme="c" style="text-align: left; padding: 10px;">
+		<center>
+			<h3>Expérimentation pour 2 mois, à partir d'avril avec un nombre limité de menbres.</h3>
+			<h2>Participez!</h2>
+		</center>
 		<form action="#" method="post" name="inscriptionForm" id="inscriptionForm">
 			<input type="hidden" name="inscription" value="1" />
-			<span>Prénom : </span><input type="text" name="prenom" value="" /><br />
-			<span>Nom : </span><input type="text" name="nom" value="" /><br />
+			<span>Prénom / Activité commerciale : </span><input type="text" name="prenom" value="" /><br />
+			<span>Nom </span><input type="text" name="nom" value="" /><br />
 			<span>eMail : </span><input type="text" name="email" value="" /><br />
 			<span>Mot de passe : </span><input type="password" name="password" /><br />
 			<span>Confirmation : </span><input type="password" name="confirm" /><br />
 			<input id="service-term" type="checkbox" name="checkCondition" style="position: absolute; top: 13px;"/>
-			<span style="position: relative; left: 50px;">J'accepte les <a href="#condition">conditions d'utilisation</a></span><br />
+			<span style="position: relative; left: 50px;">J'accepte de tester cette application et vous communique mes coordonnées<br />
 			<center>
 			<a data-role="button" onclick="document.inscriptionForm.submit()" data-theme="b" data-inline="true">Valider</a>
 			</center>
