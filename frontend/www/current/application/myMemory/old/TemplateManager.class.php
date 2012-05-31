62e7ce88b4e2887f16bf62f94a81fc9ca029c22a
David Da Silva <contact@daviddasilva.net>
2012-05-31 14:51:40 +0200

    NEW: APPLICATION : MyMemory (Alzheimer). Nothing yet, project start.

---
+++ frontend/www/current/application/myMemory/old/TemplateManager.class.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once dirname(__FILE__).'/../../lib/dasp/request/Request.class.php';
+require_once dirname(__FILE__).'/../../system/config.php';
+
+class TemplateManager {
+	
+	/**
+	 * Print header
+	 */
+	
+	public function getHeader(){ ?>
+	<!DOCTYPE html>
+		<html manifest="">
+		<head>
+			<meta charset="utf-8" />
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
+			<title>myMemory</title>
+			<link rel="stylesheet" href="http://code.jquery.com/mobile/1.1.0/jquery.mobile-1.1.0.min.css" />
+			<link rel="stylesheet" href="my.css" />
+			<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+			<script src="http://code.jquery.com/mobile/1.1.0/jquery.mobile-1.1.0.min.js"></script>
+			<script src="app.js"></script>
+		    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+    	</head>
+    	<body>
+	<?php }
+
+	
+	/*
+	 * Print Footer
+	 */
+	public function getFooter(){
+		echo "</body>";
+		echo "</html>";		
+	}
+	
+}
+?>
