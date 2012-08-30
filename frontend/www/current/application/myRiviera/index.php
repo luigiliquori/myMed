68916b98ec95dbc5591344c7cd607869b83837ec
Laurent Vanni <laurent.vanni@inria.fr>
2012-08-30 15:14:18 +0200

    REFACTORING: myRiviera v2 - use now the mvc sample _LV

--- frontend/www/current/application/myRiviera/index.php
+++ frontend/www/current/application/myRiviera/index.php
@@ -1,5 +1,14 @@
 <?php
-	define('DEBUG', true);
-	require('system/main.php');
-?>
 
+// ---------------------------------------------------------------------
+// Constants
+// ---------------------------------------------------------------------
+define('APPLICATION_NAME', "myRiviera");
+define('APP_ROOT', '.');
+define('MYMED_ROOT', '../../');
+define('MYMED_URL_ROOT', '../../');
+
+// Include main controller : Dispatches actions to individual controllers
+include(MYMED_ROOT . '/system/controllers/index-controller.php');
+
+?>
