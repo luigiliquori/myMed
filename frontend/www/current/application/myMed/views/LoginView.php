2d9de6724367435a2b9bdc2aeba7ce7779bd3d90
Laurent Vanni <laurent.vanni@inria.fr>
2012-09-05 14:52:11 +0200

    UPDATE: Lang updated _LV

--- frontend/www/current/application/myMed/views/LoginView.php
+++ frontend/www/current/application/myMed/views/LoginView.php
@@ -8,7 +8,7 @@ require_once("header.php"); ?>
 <div data-role="page" id="login">
 
 	<div data-role="header" data-theme="b">
-		<div Style="text-align: center; position: relative; top: 15px;"> Réseau Social Transfrontalier </div>
+		<div Style="text-align: center; position: relative; top: 15px;"> <?= _("Social Network") ?> </div>
 		<span class="ui-title"></span>
 		<? include("notifications.php"); ?>
 	</div>
@@ -20,13 +20,13 @@ require_once("header.php"); ?>
 		<form action="?action=login" method="post" data-ajax="false">
 			<input type="hidden" name="signin" value="1" />
 		    <input type="text" name="login" id="login" placeholder="email"  data-theme="c"/><br />
-		    <input type="password" name="password" id="password" placeholder="Mot de passe"  data-theme="c"/><br />
- 		    <input type="submit" data-role="button" data-inline="true" data-theme="b" value="Connexion" />
+		    <input type="password" name="password" id="password" placeholder="<?= _("Password") ?>"  data-theme="c"/><br />
+ 		    <input type="submit" data-role="button" data-inline="true" data-theme="b" value="<?= _("Log In") ?>" />
 		</form>
 		<br /><br />
 		<img alt="Alcotra" src="<?= MYMED_URL_ROOT ?>/system/img/logos/alcotra.png" />
 		<br />
-		<i>"Ensemble par-delà les frontières"</i>
+		<i><?= _("Ensemble par-delà les frontières") ?></i>
 		<br /><br />
 		<img alt="Alcotra" src="<?= MYMED_URL_ROOT ?>/system/img/logos/europe.jpg" />
 		
@@ -36,9 +36,9 @@ require_once("header.php"); ?>
 	<div data-role="footer" data-position="fixed" data-theme="b">
 		<div data-role="navbar">
 			<ul>
-				<li><a href="#login" data-transition="none" data-back="true" data-icon="home" class="ui-btn-active ui-state-persist">Connexion</a></li>
-				<li><a href="#register" data-transition="none" data-back="true" data-icon="grid">Inscription</a></li>
-				<li><a href="#about" data-transition="none" data-icon="info">A propos</a></li>
+				<li><a href="#login" data-transition="none" data-back="true" data-icon="home" class="ui-btn-active ui-state-persist"><?= _("Log In") ?></a></li>
+				<li><a href="#register" data-transition="none" data-back="true" data-icon="grid"><?= _("Registration") ?></a></li>
+				<li><a href="#about" data-transition="none" data-icon="info"><?= _("About") ?></a></li>
 			</ul>
 		</div>
 	</div>
