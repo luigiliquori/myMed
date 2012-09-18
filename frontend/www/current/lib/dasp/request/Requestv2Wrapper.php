37b30d1c8980acbf509bfce6c88fd946026b76c7
cyril auburtin <auburtic@mymed20.sophia.inria.fr>
2012-09-18 11:59:25 +0200

    [F] mymed Oauth OpenID

---
+++ frontend/www/current/lib/dasp/request/Requestv2Wrapper.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * Copyright 2012 INRIA
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+class Requestv2Wrapper extends Requestv2 {
+
+	public function __construct(
+			$handler=null,
+			$data= array(),
+			$method=READ,
+			$ressource = "v2/PublishRequestHandler") {
+		parent::__construct($ressource, $method, $data);
+		$this->handler = $handler;
+	}
+
+	public /*string*/ function send() {
+		try{
+			$result = parent::send();
+		}catch(Exception $e){
+		}
+		
+		$obj = json_decode($result);
+		
+		if ($obj->status == 404 && !is_null($this->handler)){
+			$this->handler->setError('');
+			throw new NoResultException("No results found");
+		
+		} else if($obj->status == 200) {// Success
+			return $obj->dataObject;
+		} else {
+			if (!is_null($this->handler))
+				$this->handler->setError($obj->description);
+			throw new Exception("Erreur");
+		}
+		
+	}
+}
+?>
---
+++ frontend/www/current/lib/socialNetworkAPIs/php-openid/examples/consumer-simple/oid_catch.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once "oid-common.php";
+
+// Starts session (needed for YADIS)
+session_start();
+
+// Create file storage area for OpenID data
+$store = new Auth_OpenID_FileStore(getStorePath());
+
+// Create OpenID consumer
+$consumer = new Auth_OpenID_Consumer($store);
+
+// Create an authentication request to the OpenID provider
+$response = $consumer->complete('http://mymed20.sophia.inria.fr/lib/socialNetworkAPIs/php-openid/examples/consumer-simple/oid_catch.php');
+
+if ($response->status == Auth_OpenID_SUCCESS) {
+	// Get registration informations
+	$ax = new Auth_OpenID_AX_FetchResponse();
+	$obj = $ax->fromSuccessResponse($response);
+	
+	//should fetch Sreg also here
+	
+	require_once ROOT.'lib/dasp/beans/MUserBean.class.php';
+	
+	$_SESSION['user'] = MUserBean::constructFromOpenId($obj->data);
+	$_SESSION['accessToken'] = sha1($_SESSION['user']->email . time());
+	header('Location: ./../../../../../?action=login');
+
+
+} else {
+	print('failed ;(');
+}
+
+
+?>
