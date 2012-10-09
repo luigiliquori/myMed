7e2aa5ea453c447d5646304301a6b4adbc528f6c
Riccardo Loti <riccardo.loti@gmail.com>
2012-10-09 04:26:47 +0200

    Last, and now correct, upload of folder iMed

---
+++ frontend/binary/iOS/iMed/iMed/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iMed
+//
+//  Created by Riccardo Loti on 09/10/12.
+//  Copyright (c) 2012 MyMed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "iMedAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([iMedAppDelegate class]));
+  }
+}
---
+++ frontend/binary/iOS/iMed/iMedTests/iMedTests.h
@@ -0,0 +1,13 @@
+//
+//  iMedTests.h
+//  iMedTests
+//
+//  Created by Riccardo Loti on 09/10/12.
+//  Copyright (c) 2012 MyMed. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface iMedTests : SenTestCase
+
+@end
