4c994701e62e8055e24842a594e08f5ed0bc2a6e
Laurent Vanni <laurent.vanni@inria.fr>
2012-10-15 14:08:47 +0200

    NEW: IeView added to system/common

---
+++ frontend/www/current/system/common/IeView.php
@@ -0,0 +1,6 @@
+<html>
+Vous utilisez internet explorer, un navigateur qui ne respecte pas les standard de l'internet.<br />
+Ce site est optmis&eacute; pour un grand nombre de navigateurs tels que <a href="http://www.mozilla.org/fr/firefox/fx/">Firefox</a><br/>
+ou <a href="http://www.google.com/intl/fr/chrome/browser/">google chrome</a>. <br />
+</html>
+
