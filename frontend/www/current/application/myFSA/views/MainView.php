fddae7c117f1d9bfcdb55a94eb17a2513c4a5e5a
Laurent Vanni <laurent.vanni@inria.fr>
2012-10-26 11:32:46 +0200

    NEW: myFSA - Search/Publish/Localise moved to tabBar

--- frontend/www/current/application/myFSA/views/MainView.php
+++ frontend/www/current/application/myFSA/views/MainView.php
@@ -1,18 +1,60 @@
 <? include("header.php"); ?>
-
-
-<div data-role="page" data-theme="b">
-<? include("header-bar.php"); ?>
-
-
- 	<div data-role="content" style="padding: 15px" data-ajax="false">
-	<a data-ajax="false" href="controllers/LocalisationController.php" type="button" data-transition="slide" data-icon="info"><?= translate("Localise") ?></a>
-	<a data-role="button" data-transition="fade" href="?action=Publish" data-icon="plus"> <?= translate("Publish") ?></a>
-	<form action="index.php?action=publish" method="POST" data-ajax="false">
-	<input type="hidden" name="method" value="Rechercher" />
-	<input type="submit" value="<?= translate('Search') ?>" data-icon="search"/>
-	</form>
-	</div>
+<div data-role="page" id="Search" data-theme="b">
+	<? include("header-bar.php"); ?>
+		
+		<div data-role="content">
+			<br />
+			<ul data-role="listview" data-filter="true" data-inset="true" data-filter-placeholder="...">
+				<!-- petla koncze! -->		
+				<? if (count($this->result) == 0) :?>
+				<li>
+					<h4>No result found</h4>
+				</li>
+				<? endif ?>
+				<?// $_SESSION['publication'] = $this->result; ?>
+				<? foreach($this->result as $item) : ?>
+				<li><a data-ajax="false" href="?action=details&predicate=<?= $item->getPredicateStr() ?>&author=<?= $item->publisherID ?>">	
+					<?php $truncated = substr($item->publisherID, 6)	?>	
+					<!--<b>Publisher ID</b> : --><?=  $truncated ?>,
+					<!--<b>Pred1</b>: --><?//= $item->pred1 ?>
+					<!--<b>Pred2</b>: --><?= $item->pred2 ?>,
+					<!--<b>Pred3</b>: --><?= $item->pred3 ?>
+					<!--<b>Begin</b>: --><?//= $item->begin ?>
+					<!--<b>End</b>: --><?//= $item->end ?>
+					<!--<b>Wrapped1</b>: --><?//= $item->wrapped1 ?><br/>
+					<!--<b>Wrapped2</b>: --><?//= $item->wrapped2 ?><br/>
+				</a></li>
+				<? endforeach ?>
+				<!-- petla koncze! -->
+			</ul>
+				
+				<div data-role="collapsible" data-collapsed="true" data-theme="b" data-icon="plus">
+				<h3><?= translate('Advanced searching') ?></h3>
+				<form action="index.php?action=publish" method="POST" data-ajax="false">
+				
+					<div data-role="fieldcontain" style="margin-left: auto;margin-right: auto;">
+						<fieldset data-role="controlgroup" >
+							<label for="textinputs1"> <?= translate('Cathegory') ?> </label> 
+							<input id="textinputs1" name="pred2" placeholder="" type="text" />
+						</fieldset>
+					</div>
+					
+					<div data-role="fieldcontain" style="margin-left: auto;margin-right: auto;">
+						<fieldset data-role="controlgroup" >
+						<label for="textinputs2"> <?= translate('Tittle') ?> </label> 
+						<input id="textinputs2"  name="pred3" placeholder="" type="text" />
+						</fieldset>
+					</div>
+						
+						<input type="hidden" name="method" value="Rechercher" />
+						<input type="submit" value="<?= translate('Search') ?>" data-icon="gear"/>
+						</form>
+				</div>	
+				
+				
+				<div class="push"></div>
+				
+			</div>
 	
 <? include("footer.php"); ?>
 </div>
--- frontend/www/current/application/myFSA/views/parts/footer.php
+++ frontend/www/current/application/myFSA/views/parts/footer.php
@@ -1,7 +1,9 @@
 	<div data-role="footer" data-position="fixed" data-theme="d">
 		<div data-role="navbar">
 			<ul>
-				<li><a href="?action=main" data-transition="none" data-icon="home" <?= $_GET['action'] == "main" || !isset($_GET['action']) ? 'data-theme="b"' : '' ?>><?= translate("Home") ?></a></li>
+				<li><a href="?action=main" data-transition="none" data-icon="home" <?= $_GET['action'] == "main" || !isset($_GET['action']) ? 'data-theme="b"' : '' ?>><?= translate("Search") ?></a></li>
+				<li><a data-role="button" data-transition="fade" href="?action=Publish" data-icon="plus"> <?= translate("Publish") ?></a></li>
+				<li><a data-ajax="false" href="controllers/LocalisationController.php" type="button" data-transition="slide" data-icon="info"><?= translate("Localise") ?></a></li>
 				<li><a href="?action=ExtendedProfile" data-icon="user" <?= $_GET['action'] == "ExtendedProfile" ? 'data-theme="b"' : '' ?> ><?= translate("Profile") ?></a></li>
 			</ul>
 		</div>
