fca7729e87038462bae1a58fbd42ddb92be49ebb
cyril auburtin <auburtic@mymed20.sophia.inria.fr>
2012-10-30 19:03:55 +0100

    [B,F] new blog templates and europe view

---
+++ frontend/www/current/application/myEurope/views/ExtendedProfileListView.php
@@ -0,0 +1,36 @@
+<? include("header.php"); ?>
+
+<div data-role="page" id="profiles">
+
+	
+	<? tabs_simple(array('Profiles')); ?>
+	<? include("notifications.php"); ?>
+
+	<div data-role="content">
+		<br>
+		<? if ($_SESSION['myEurope']->permission <= 0): ?>
+			<div data-role="header" data-theme="e">
+			
+			<h1 style="white-space: normal;"><?= _("Hello, This is your first time on myEurope! Please register with an already existing profile or create yours") ?></h1>
+			</div>
+			<br />
+		<? endif; ?>
+		<a href="?action=ExtendedProfile&new" type="button" data-inline="true" data-mini="true" data-theme="e" data-icon="pencil" style="float: right;"><?= _("Create") ?></a>
+		
+		<ul data-role="listview" data-filter="true" data-inset="true" data-mini="true" data-filter-placeholder="<?= _("filter") ?>">
+		<? foreach ($this->cats as $k=>$v) :?>
+			<? if (!empty($v)) :?>
+				<li data-role="list-divider"><?= Categories::$roles[$k] ?></li>
+				<? foreach ($v as $ki=>$vi) :?>
+					<li><a href="?action=ExtendedProfile&id=<?= $vi->id ?>&link"><?= $vi->name ?></a></li>
+				<? endforeach ?>
+			<? endif ?>
+		<? endforeach ?>
+		</ul>
+	</div>
+	
+</div>
+
+
+<? include("footer.php"); ?>
+
