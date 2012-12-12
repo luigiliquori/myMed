1032ffa606d9a808f6047a9e82264be2cc6d3ffb
Laurent Vanni <laurent.vanni@inria.fr>
2012-12-12 11:29:57 +0100

    NEW: myEurope: small update of the details view _LV

--- frontend/www/current/application/myEurope/views/DetailsView.php
+++ frontend/www/current/application/myEurope/views/DetailsView.php
@@ -11,31 +11,12 @@
 		<div data-role="collapsible-set" data-theme="c" data-content-theme="d">
 
 			<div data-role="collapsible" data-collapsed="false">
-				
-				<?if ($this->result->publisherID == $_SESSION['user']->id){ ?>
-					<form action="index.php?action=main" method="POST" data-ajax="false">
-						<input type="hidden" name="theme" value="<?= $this->result->theme ?>" />
-						<input type="hidden" name="other" value="<?= $this->result->other ?>" />
-		 				<input type="hidden" name="method" value="Delete" />
-						<input type="submit" data-icon="delete" data-theme="r" data-inline="true" data-mini="true" value="<?= translate('Delete publication') ?>" />
-		 			</form>
-				<? } ?> 
 			
-				<h3><?= $this->result->title ?></h3>
+			<br />	
+			<h3><?= _("Description") ?></h3>
 								
-				<p style="position: relative; margin-left: 30px;">
-					<b><?= _('keywords') ?></b>: <?= $this->result->theme ?>, <?= $this->result->other ?><br/>
-					<b>End</b>: <?= $this->result->end ?><br/><br />
-				</p>
-					
-				<h3><?= $this->result->title ?> :</h3>
-				<?= $this->result->text ?><br/><br />
-				
-				<br/>
-				
-				<p>
-					<b>Contact</b>: <?= str_replace("MYMED_", "", $this->result->publisherID) ?><br/>
-					Reputation: 
+	    		<!-- REPUTATION -->
+	    		<div style="position: absolute; top:110px; right: 25px;">
 					<a href="#popupReputationProject" data-rel="popup">
 						<?php for($i=1 ; $i <= 5 ; $i++) { ?>
 							<?php if($i*20-20 < $this->reputation["value"] ) { ?>
@@ -45,8 +26,7 @@
 							<?php } ?>
 						<? } ?>
 					</a>
-				</p>
-				
+				</div>
 				<div data-role="popup" id="popupReputationProject" class="ui-content" Style="text-align: center; width: 18em;">
 					<?= _("Do you like the project idea ?") ?><br /><br />
 					<form action="#" method="get" data-ajax="false">
@@ -58,58 +38,60 @@
 						<input type="submit" value="Send" data-mini="true" data-theme="g"/>
 					</form>
 				</div>
+				<!-- END REPUTATION -->
+	    		
+				<div>
+					<!-- TITLE -->
+					<h3><?= $this->result->title ?> :</h3>
+					
+					<!-- TEXT -->
+					<?= $this->result->text ?>
 				
-				<br/>
+					<!-- CONTACT -->			
+					<p><b>Contact</b>: <?= str_replace("MYMED_", "", $this->result->publisherID) ?><br/></p>
+					
+					<!-- Keywords an timeline
+					<p style="position: relative; margin-left: 30px;">
+						<b><?= _('keywords') ?></b>: <?= $this->result->theme ?>, <?= $this->result->other ?><br/>
+						<b>End</b>: <?= $this->result->end ?><br/><br />
+					</p> -->
+					
+					<!-- DELETE -->
+					<?if ($this->result->publisherID == $_SESSION['user']->id){ ?>
+						<form action="index.php?action=main" method="POST" data-ajax="false">
+							<input type="hidden" name="theme" value="<?= $this->result->theme ?>" />
+							<input type="hidden" name="other" value="<?= $this->result->other ?>" />
+			 				<input type="hidden" name="method" value="Delete" />
+							<input type="submit" data-icon="delete" data-theme="r" data-inline="true" data-mini="true" value="<?= translate('Delete publication') ?>" />
+			 			</form>
+					<? } ?> 
 				
-				<p>
-					<b>Contact</b>: <?= $this->result->publisherID ?><br/>
-					Reputation: 
-					<a href="#popupReputation2" data-rel="popup">
-						<?php for($i=1 ; $i <= 5 ; $i++) { ?>
-							<?php if($i*20-20 < $this->reputation["author"] ) { ?>
-								<img alt="rep" src="img/yellowStar.png" width="12" Style="left: <?= $i ?>0px; margin-top:3px;" />
-							<?php } else { ?>
-								<img alt="rep" src="img/grayStar.png" width="12" Style="left: <?= $i ?>0px; margin-top:3px;"/>
-							<?php } ?>
-						<? } ?>
-						</a>
-				</p>
-				
-				<div data-role="popup" id="popupReputation2" class="ui-content" Style="text-align: center;">
-					<?= _("Do you like the content?") ?><br /><br />
-					<a href="?action=updateReputation&reputation=10&predicate=<?= $_GET['predicate'] ?>&author=<?= $_GET['author'] ?>" data-mini="true" data-role="button" data-inline="true" rel="external" data-theme="g" data-icon="plus">Of course yes!</a><br />
-					<a href="?action=updateReputation&reputation=0&predicate=<?= $_GET['predicate'] ?>&author=<?= $_GET['author'] ?>" data-mini="true" data-role="button" data-inline="true" rel="external" data-theme="r" data-icon="minus">No, not really...</a>
 				</div>
 				
-				<a href="#popupShare" data-rel="popup" data-role="button" data-theme="b">Share</a>
-			
-				<div data-role="popup" id="popupShare" class="ui-content" Style="text-align: center;" >
-					<?= _("Share this on :")?><br/>
-					<div class="ui-grid-b">
-						<div class="ui-block-a">
-							<a href="http://twitter.com/share" class="twitter-share-button" data-count="vertical" data-via="my_Europe" data-url="<?= str_replace('@','%40','http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'])?>">Tweet</a>
-    						<script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
-   						</div>
-						<div class="ui-block-b">
-							<script type="text/javascript" src="https://apis.google.com/js/plusone.js">
-    							{lang: 'en';}
-    						</script>
-    						<g:plusone size="tall"></g:plusone>
-    					</div>
-						<div class="ui-block-c">
-							<a name="fb_share" type="box_count" share_url="<?= 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']?>"></a>
-   							<script src="http://static.ak.fbcdn.net/connect.php/js/FB.Share" type="text/javascript"></script>
-						</div>
+				<!-- SHARE THIS -->
+				<div style="position: absolute; right: 24px;">
+				<a href="http://twitter.com/share" class="twitter-share-button" data-count="vertical" data-via="my_Europe" data-url="<?= str_replace('@','%40','http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'])?>">Tweet</a>
+    				<script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
 					</div>
 					
-    			</div>
+					<div style="position: absolute; right: 95px;">
+				<g:plusone size="tall"></g:plusone>
+				<script type="text/javascript" src="https://apis.google.com/js/plusone.js">{lang: 'en';}</script>
+				</div>
+				
+				<div style="position: absolute; right: 150px;">
+				<a name="fb_share" type="box_count" share_url="<?= 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']?>" ></a>
+   				<script src="http://static.ak.fbcdn.net/connect.php/js/FB.Share" type="text/javascript"></script>
+				</div>
+				<div style="height: 80px;"></div>
+	    		<!-- END SHARE THIS -->
 				
 			</div>
 		
 		</div>
-	
+		
 	</div>
 	
 </div>
+
 <? include_once 'MainView.php'; ?>
-<!-- <iframe src="http://www.facebook.com/plugins/like.php?href=<?= 'http://' . $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'] ?>" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:65px; height:65px; margin-top:3px;" allowTransparency="true"></iframe>-->
