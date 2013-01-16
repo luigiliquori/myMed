bb00b3a0ecdf836c47227407e7a14edd5ab5aff1
Salvatore Spoto <salvatore.spoto@gmail.com>
2013-01-16 14:39:58 +0100

    [FIX] Fix merge with rfritz-dev

--- frontend/www/current/application/myEurope/views/parts/header-bar.php
+++ frontend/www/current/application/myEurope/views/parts/header-bar.php
@@ -5,28 +5,24 @@
  * @param boolean $print_logout_button, add the logout button to the header bar
  * 				
  */
-function print_header_bar($print_back_button = false, $idHelpPopup = "defaultHelpPopup", $print_logout_button = false) {
+
+function print_header_bar($print_back_button = false, $idHelpPopup = "defaultHelpPopup", $title, $print_logout_button = false) {
 	echo '<div data-role="header" data-theme="b" data-position="fixed">';
 	
 	// Pring logout button
-	if($print_logout_button ){
-			echo '<a href="?action=logout"
-			data-inline="true"
-			rel="external"
-			data-role="button"
-			data-theme="r"
-			data-icon="off"
-			data-iconpos="notext">' . _('Logout') . '</a>';
+	if($print_logout_button && !($_SESSION['user']->is_guest)) {
+		echo '<a href="?action=logout" class="ui-btn-left" data-role="button" rel="external" data-icon="off" data-iconpos="notext" data-theme="r">Sign out</a>';
 	}
-		
+		  
 	if($print_back_button) {
 		
 		echo '<a data-rel="back" data-icon="arrow-left">' . _("Back") . '</a>';
 	} else {
 // 		include 'social.php';
 	}
+	$title=empty($title)?APPLICATION_NAME:$title;
 	
-	echo '<h1>' . APPLICATION_NAME . '</h1>';
+	echo '<h1>' .$title. '</h1>';
 	
 	if($idHelpPopup) {
 		echo '<a href="#' . $idHelpPopup . '" id="openHelp" data-icon="question-sign" class="ui-btn-right" data-theme="e" data-rel="popup">' . _('Help') . '</a>';
