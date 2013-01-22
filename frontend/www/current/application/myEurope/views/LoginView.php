8954af1c9d2c4d3263437a442d77f0cca6ffe146
romain <romain.fritz@inria.fr>
2013-01-22 11:18:53 +0100

    Fixing register view Romain and Elsa

--- frontend/www/current/application/myEurope/views/LoginView.php
+++ frontend/www/current/application/myEurope/views/LoginView.php
@@ -31,7 +31,7 @@
 				    <div data-role="controlgroup" data-type="horizontal">
 				 	    <input type="submit" data-role="button" data-mini="true" data-inline="true" data-theme="b" data-icon="signin" value="<?= _("Sign in") ?>" />
 						<a href="#signinPopup" data-role="button" data-rel="popup" data-inline="true" data-mini="true" data-icon="star"><?= _("Sign in with") ?></a>
-						<a href="#register" data-role="button" data-inline="true" data-mini="true" data-icon="pencil" data-iconpos="right"><?= _("Register") ?></a>	
+						<a href="?action=register" data-role="button" data-inline="true" data-mini="true" data-icon="pencil" data-iconpos="right"><?= _("Register") ?></a>	
 					</div>
 				    
 			    </div>
@@ -70,53 +70,3 @@
 	</div>
 	
 </div>
-	
-<div data-role="page" id="register">
-	<? $title = _("Register");
-	print_header_bar(true, false, $title); ?>
-	
-<!--<div data-role="header" data-theme="b">
-		<h1><?= _("Account creation") ?></h1>
-	</div>
- -->
-	<div data-role="content">
-	
-		<? print_notification($this->success.$this->error); ?>
-	
-		<!--  Register form -->
-		<form action="?action=register" method="post" data-ajax="false">
-		
-				<label for="prenom"><?= _("First name") ?>: </label>
-				<input type="text" name="prenom" value="" />
-				<br />
-				
-				<label for="nom"><?= _("Last name") ?>: </label>
-				<input type="text" name="nom" value="" />
-				<br />
-				
-				<label for="email" >Email: </label>
-				<input type="text" name="email" value="" />
-				<br />
-				
-				<label for="password" ><?= _("Password") ?>: </label>
-				<input type="password" name="password" />
-				<br />
-				
-				<label for="password" ><?= _("Confirm") ?>: </label>
-				<input type="password" name="confirm" />
-				<br />
-				
-				<input id="service-term" type="checkbox" name="checkCondition" style="position: absolute; top: 13px;"/>
-				<span style="position: relative; left: 50px;">
-					<a href="application/myMed/doc/CGU_fr.pdf" rel="external"><?= _('I accept the general terms and conditions'); ?></a>
-				</span><br>
-				
-				<div style="text-align: center;">
-					<input type="submit" data-role="button" data-theme="g" data-inline="true" data-mini="true" data-icon="ok-sign" value="<?= _('Send') ?>" />
-				</div>
-		
-		</form>
-	</div>
-	
-</div>
-
--- frontend/www/current/application/myEurope/views/RegisterView.php
+++ frontend/www/current/application/myEurope/views/RegisterView.php
@@ -1,62 +1,51 @@
 <? require_once("header.php"); ?>
 <? include("notifications.php"); ?>
 
-<div data-role="page" id="register" >
+<div data-role="page" id="register">
 	<? $title = _("Register");
-	 print_header_bar(true, false, $title); ?>
-
+	print_header_bar(true, false, $title); ?>
+	
 <!--<div data-role="header" data-theme="b">
-		<h1><?=// _("Account creation") ?></h1>	
+		<h1><?= _("Account creation") ?></h1>
 	</div>
  -->
 	<div data-role="content">
 	
+		<? print_notification($this->success.$this->error); ?>
+	
 		<!--  Register form -->
-		<form action="index.php?action=register" method="post" data-ajax="false">
+		<form action="?action=register" method="post" data-ajax="false">
 		
-				<label for="prenom"><?= _("First name")?> : </label>
+				<label for="prenom"><?= _("First name") ?>: </label>
 				<input type="text" name="prenom" value="" />
 				<br />
 				
-				<label for="nom"><?= _("Last name")?> :</label>
+				<label for="nom"><?= _("Last name") ?>: </label>
 				<input type="text" name="nom" value="" />
 				<br />
 				
-				<label for="email" >eMail : </label>
+				<label for="email" >Email: </label>
 				<input type="text" name="email" value="" />
 				<br />
 				
-				<label for="password" ><?= _("Password")?> :</label>
+				<label for="password" ><?= _("Password") ?>: </label>
 				<input type="password" name="password" />
 				<br />
 				
-				<label for="password" ><?= _("Password Confirmation")?> :</label>
+				<label for="password" ><?= _("Confirm") ?>: </label>
 				<input type="password" name="confirm" />
 				<br />
 				
 				<input id="service-term" type="checkbox" name="checkCondition" style="position: absolute; top: 13px;"/>
 				<span style="position: relative; left: 50px;">
-					J'accepte les 
-					<a href="<?= MYMED_URL_ROOT ?>/application/myRiviera/system/templates/application/myRiviera/doc/CONDITIONS_GENERALES_MyMed_version1_FR.pdf" rel="external">conditions d'utilisation</a> / 
-					I accept 
-					<a href="<?= MYMED_URL_ROOT ?>/application/myRiviera/system/templates/application/myRiviera/doc/CONDITIONS_GENERALES_MyMed_version1_EN.pdf" rel="external">the general terms and conditions</a>
-				</span><br />
+					<a href="application/myMed/doc/CGU_fr.pdf" rel="external"><?= _('I accept the general terms and conditions'); ?></a>
+				</span><br>
 				
-				<center>
-					<input type="submit" data-role="button" data-theme="b" data-inline="true" value=<?= _("Validate")?> />
-				</center>
+				<div style="text-align: center;">
+					<input type="submit" data-role="button" data-theme="g" data-inline="true" data-mini="true" data-icon="ok-sign" value="<?= _('Send') ?>" />
+				</div>
 		
 		</form>
-	</div>
-	
-	<div data-role="footer" data-position="fixed" data-theme="d">
-		<div data-role="navbar">
-			<ul>
-				<li><a href="#login" data-transition="none" data-back="true" data-icon="home"><?= _("Sign in")?></a></li>
-				<li><a href="#register" data-transition="none" data-back="true" data-icon="grid"  class="ui-btn-active ui-state-persist"><?= _("Registration")?></a></li>
-				<li><a href="#about" data-transition="none" data-icon="info"><?= _("About")?></a></li>
-			</ul>
-		</div>
 	</div>
 	
 </div>
