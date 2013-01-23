1aa4bb47ca44be945bb356d1ea0507d8b69986b6
erol <elsa.rol@inria.fr>
2013-01-23 09:45:57 +0100

    commit myOpportunity and find view

--- frontend/www/current/application/myEdu/controllers/DetailsController.class.php
+++ frontend/www/current/application/myEdu/controllers/DetailsController.class.php
@@ -7,12 +7,21 @@ class DetailsController extends AuthenticatedController {
 		
 		parent::handleRequest();
 		
+		/* Guest access provided */
+		if (!(isset($_SESSION['user'])) && $_SESSION['user']->is_guest) {
+			$id = rand(100000, 999999);
+			$user = (object) array('id'=>'MYMED_'.$id, 'name'=>'user'.$id);
+			$_SESSION['user'] = insertUser($user, null, true);
+			$_SESSION['acl'] = array('defaultMethod', 'read');
+			$_SESSION['user']->is_guest = 1;
+		}
+		
 		// Get arguments of the query
 		$predicate = $_GET['predicate'];
 		$author = $_GET['author'];
 		
 		// Create an object
-		$obj = new ExampleObject($predicate);
+		$obj = new MyEduPublication($predicate);
 		$obj->publisherID = $author;
 		
 		// Fetches the details
@@ -20,22 +29,24 @@ class DetailsController extends AuthenticatedController {
 		 
 		// Give this to the view
 		$this->result = $obj;
-		
 		// get author reputation
 		$request = new Request("ReputationRequestHandler", READ);
 		$request->addArgument("application",  APPLICATION_NAME);
 		$request->addArgument("producer",  $obj->publisherID);							
-		$request->addArgument("consumer",  $_SESSION['user']->id);
+		$request->addArgument("consumer",  $obj->publisherID);
 		
-// 		$responsejSon = $request->send();
-// 		$responseObject = json_decode($responsejSon);
+		$responsejSon = $request->send();
+		$responseObject = json_decode($responsejSon);
 		
-// 		if (isset($responseObject->data->reputation)) {
-// 			$value =  json_decode($responseObject->data->reputation) * 100;
-// 		} else {
-// 			$value = 100;
-// 		}
-// 		$this->reputation["author"] = $value;
+		if (isset($responseObject->data->reputation)) {
+			$value =  json_decode($responseObject->data->reputation) * 100;
+		} else {
+			$value = 100;
+		}
+		
+		// Save the reputation and the number of voters
+		$this->reputation["author"] = $value;
+		$this->reputation["author_noOfRatings"] = $responseObject->dataObject->reputation->noOfRatings;
 		
 		// get value reputation
 		$request->addArgument("producer",  $predicate.$obj->publisherID);	
@@ -48,7 +59,10 @@ class DetailsController extends AuthenticatedController {
 		} else {
 			$value = 100;
 		}
+		
+		// Save the reputation and the number of ratings
 		$this->reputation["value"] = $value;
+		$this->reputation["value_noOfRatings"] = $responseObject->dataObject->reputation->noOfRatings;
 		
 		// Render the view
 		$this->renderView("details");
