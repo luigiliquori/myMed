f2b413fd062f2a4338584e90cf5a9e935ec072bb
David Da Silva <contact@daviddasilva.net>
2013-01-25 00:40:02 +0100

    mymemory

--- frontend/www/current/application/myMemory/views/MainView.php
+++ frontend/www/current/application/myMemory/views/MainView.php
@@ -1,18 +1,63 @@
 <? include("notifications.php")?>
 
 <div data-role="page" id="MainView">
-
+<script type="text/javascript">
 	<?php
 	$_SESSION['autocall_active'] = true;
 	
-
-
+	if(($_SESSION['ExtendedProfile']->diseaseLevel == 3) && $_SESSION['isMobile'] ){
+		/*
+		 * WE ACTIVATE AUTOCALL
+		 */
+		
+		/*
+		 * URL SCHEMA :
+		*
+		* 0 : mobile_binary
+		* 1 : guardian
+		* 2 : person name		(string)
+		* 3 : home 				(string)
+		* 5 : perimetre_home	(float)
+		* 6 : perimetre_nearby (float)
+		* 7 : perimetre_far	(float)
+		* 8 : tel_buddy1		(string)	usually caregiver phone
+		* 9 : tel_buddy2		(string)	usually doctor
+		* 10 : tel_buddy3		(string)	usually samu
+		* 11: interval			(int)		in minutes
+		*/
+		
+		if($_SESSION['autocall_active'] == false) {
+		
+			$guardian_params = '';
+			
+			// person name
+			$guardian_params .= $_SESSION['user']->name."::";
+			// home
+			$guardian_params .= $_SESSION['ExtendedProfile']->home."::";
+			// perimeter home
+			$guardian_params .= $_SESSION['ExtendedProfile']->perimeter_home."::";
+			// perimeter nearby
+			$guardian_params .= $_SESSION['ExtendedProfile']->perimeter_nearby."::";
+			// perimeter far
+			$guardian_params .= $_SESSION['ExtendedProfile']->perimeter_far."::";
+			// buddy 1
+			$guardian_params .= $_SESSION['ExtendedProfile']->callingListAutoCall['0']."::";
+			// buddy 2
+			$guardian_params .= $_SESSION['ExtendedProfile']->callingListAutoCall['1']."::";
+			// buddy 3
+			$guardian_params .= $_SESSION['ExtendedProfile']->callingListAutoCall['2']."::";
+			// interval
+			$guardian_params .= $_SESSION['ExtendedProfile']->autocall_frequency;
+			
+			$_SESSION['autocall_active'] = true;
+			
+			echo 'setTimeout(function() {location.href="/application/'.APPLICATION_NAME.'/index.php?action=main&mobile_binary::guardian::'.$guardian_params.'";},5000);';
+		}
+	}
 	?>
-
-
-<script type="text/javascript">
-	setTimeout(function() {location.href="/application/myMemory/index.php?action=main&mobile_binary::guardian::test";},5000);
 </script>
+
+
 	<!-- Header -->
 	<div data-role="header" data-position="inline">
 		<a href="../../index.php" rel="external" data-role="button" data-theme="r" class="ui-btn-left" data-icon="delete"><?= _("Exit"); ?></a>
