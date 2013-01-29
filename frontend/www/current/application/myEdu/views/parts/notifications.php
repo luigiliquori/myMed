c65c4049fd04ea24a733cd562b9b53aefa1a2316
Salvatore Spoto <salvatore.spoto@gmail.com>
2013-01-29 16:07:09 +0100

    [FIX] Fix bug in notification.php

--- frontend/www/current/application/myEdu/views/parts/notifications.php
+++ frontend/www/current/application/myEdu/views/parts/notifications.php
@@ -11,12 +11,15 @@ function print_notification($message) {
 		echo '<div data-role="popup" id="notificationPopup" data-transition="flip" data-theme="e" class="ui-content"><p>' . $message . ' </p></div>';
 		echo '
 		<script type="text/javascript">
-		
+			var first_pageshow = true;
 			$(document).on("pageshow", function() {
-			  	// Go back in the history has problem in Chrome. 
-			  	// Do not return back in the history when the pop up disappear.
-			  	$( "#notificationPopup" ).popup({ history: false });
-	    		$( "#notificationPopup" ).popup("open");
+				if(first_pageshow) {
+				  	// Go back in the history has problem in Chrome. 
+				  	// Do not return back in the history when the pop up disappear.
+				  	$( "#notificationPopup" ).popup({ history: false });
+		    		$( "#notificationPopup" ).popup("open");
+		    		first_pageshow = false;
+	    		}
 	     	});
 		
 		 </script>
