398dbb809d50815a9fa9bb4a803138ae769df19f
romain <romain.fritz@inria.fr>
2013-02-21 14:47:39 +0100

    commiting lost file

---
+++ frontend/www/current/application/myBenevolat/models/MyBenevolatSubscriptionBean.class.php
@@ -0,0 +1,50 @@
+<?php
+
+class MyBenevolatSubscriptionBean extends GenericDataBean{
+	/** Some predicates .
+	 * Register them in the contructor with appropriate ontologyID */
+	
+	public $publisher;
+	public $pubTitle;
+	public $type;
+	
+	/**
+	 * Some fields, not part of predicates, but wrapped in "_data"
+	 * to be fetched by a "search" query (no need to "details").
+	 * Register them in the contructor (3rd array)
+	 */
+	public $competence;
+	public $mobility;
+	public $mission;
+	
+	/** Some data (got after a "details" query).
+	 *  Register them in the contructor with appropriate ontologyID */
+	public $data1;
+	public $data2;
+	public $data3;
+	
+	/** Register the attributes either as predicates / data fields */
+	public function __construct(
+			$predicateStr = null /** Optional: Predicate string (=ID) */)
+	{
+	parent::__construct(
+	
+			// Predicate attributes
+	array(
+			"pubTitle" => KEYWORD,
+			"publisher"=> KEYWORD,
+			"type"=> KEYWORD),
+	
+			// Data attributes
+			array(
+					"data1" => TEXT,
+					"data2" => TEXT,
+					"data3" => ENUM),
+	
+					// Wrapped attributes
+					array("competence","mobility","mission"),
+	
+					$predicateStr);
+	
+	}
+}
