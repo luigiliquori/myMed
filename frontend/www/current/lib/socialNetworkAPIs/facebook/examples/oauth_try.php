396d699cb700677c8388cc84f4f072a790bae920
erol <elsa.rol@inria.fr>
2013-02-21 12:00:00 +0100

    commit add www or without www socailNetworks connection

--- frontend/www/current/lib/socialNetworkAPIs/facebook/examples/oauth_try.php
+++ frontend/www/current/lib/socialNetworkAPIs/facebook/examples/oauth_try.php
@@ -1,12 +1,7 @@
 <?php
-
-require_once "oauth-common.php";
-
 session_start();
 
-debug("try");
-
-$_SESSION['state'] = md5(uniqid(rand(), TRUE)); //CSRF protection
+require_once "oauth-common.php";
 
 if(strpos($_SERVER['SERVER_NAME'], 'www')==false){ // http://mymed.fr
 	define('Facebook_APP_ID', '263064853727560');
@@ -16,6 +11,7 @@ if(strpos($_SERVER['SERVER_NAME'], 'www')==false){ // http://mymed.fr
 	define('Facebook_APP_SECRET', 'c46c7bbbdf2c83990b7858ecb5c9e53c');
 }
 
+$_SESSION['state'] = md5(uniqid(rand(), TRUE)); //CSRF protection
 
 $dialog_url = "https://www.facebook.com/dialog/oauth?client_id=". Facebook_APP_ID 
 . "&redirect_uri=" . urlencode(getReturnTo()) 
